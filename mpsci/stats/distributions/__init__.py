27982b4d3df4a97d7f7d943d9c7ea7fe9db7aba1
Warren Weckesser <warren.weckesser@gmail.com>
2018-11-30 15:54:19 -0500

    ENH: Add some functions for the Rice distribution.

--- mpsci/stats/distributions/__init__.py
+++ mpsci/stats/distributions/__init__.py
@@ -1,7 +1,9 @@
+
 from . import exponweib
 from . import gamma
 from . import genextreme
 from . import ncx2
 from . import normal
+from . import rice
 
 from . import hypergeometric
---
+++ mpsci/stats/distributions/rice.py
@@ -0,0 +1,50 @@
+"""
+This module defines functions for the Rice distribution.
+"""
+
+import mpmath
+from ...fun import marcumq, cmarcumq
+
+
+__all__ = ['pdf', 'cdf', 'sf']
+
+
+# Parameter names and formulas are from the wikipedia article:
+#     https://en.wikipedia.org/wiki/Rice_distribution
+# scipy has a different parametrization:
+# Here                  Scipy
+# pdf(x, nu, sigma)     scipy.stats.rice.pdf(x, nu/sigma, loc=0, scale=sigma)
+
+def pdf(x, nu, sigma):
+    """
+    PDF for the Rice distribution.
+    """
+    x = mpmath.mpf(x)
+    nu = mpmath.mpf(nu)
+    sigma = mpmath.mpf(sigma)
+    sigma2 = sigma**2
+    p = ((x / sigma2) * mpmath.exp(-(x**2 + nu**2)/(2*sigma2)) *
+         mpmath.besseli(0, x*nu/sigma2))
+    return p
+
+
+def cdf(x, nu, sigma):
+    """
+    CDF for the Rice distribution.
+    """
+    x = mpmath.mpf(x)
+    nu = mpmath.mpf(nu)
+    sigma = mpmath.mpf(sigma)
+    c = cmarcumq(1, nu/sigma, x/sigma)
+    return c
+
+
+def sf(x, nu, sigma):
+    """
+    Survival function for the Rice distribution.
+    """
+    x = mpmath.mpf(x)
+    nu = mpmath.mpf(nu)
+    sigma = mpmath.mpf(sigma)
+    s = marcumq(1, nu/sigma, x/sigma)
+    return s
