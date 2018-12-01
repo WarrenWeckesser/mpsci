80adfdaf988382fdb77dd09b7477cd8bf2090f9f
Warren Weckesser <warren.weckesser@gmail.com>
2018-12-01 14:19:59 -0500

    Finish moving boxcox and boxcox1p to fun.

--- mpsci/fun/__init__.py
+++ mpsci/fun/__init__.py
@@ -1,4 +1,5 @@
 
+from ._boxcox import *
 from ._marcumq import *
 from ._digammainv import *
 from ._xlogy import *
--- mpsci/stats/__init__.py
+++ mpsci/stats/__init__.py
@@ -1,5 +1,4 @@
 
 from ._goftests import *
 from ._pearsonr import pearsonr
-from ._boxcox import *
 from . import distributions
