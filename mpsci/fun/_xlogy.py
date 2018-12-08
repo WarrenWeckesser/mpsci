ccac1aa2b39eea5d74a4d69f7c5fea779193e1c1
Warren Weckesser <warren.weckesser@gmail.com>
2018-12-08 10:02:33 -0500

    Handle nan in xlogy.

--- mpsci/fun/_xlogy.py
+++ mpsci/fun/_xlogy.py
@@ -1,12 +1,19 @@
 
-import mpmath
+from mpmath import mp
 
 
 __all__ = ['xlogy']
 
 
 def xlogy(x, y):
+    """
+    x*log(y)
+
+    If x is 0 and y is not nan, 0 is returned.
+    """
+    if mp.isnan(x) or mp.isnan(y):
+        return mp.nan
     if x == 0:
-        return mpmath.mp.zero
+        return mp.zero
     else:
-        return x * mpmath.log(y)
+        return x * mp.log(y)
