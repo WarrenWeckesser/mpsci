c6f6441e02a1d999df941dc5f5f5fa567885befc
Warren Weckesser <warren.weckesser@gmail.com>
2019-04-12 20:35:20 -0400

    Clean up hypergeometric distr. API, and add a unit test.

--- mpsci/stats/distributions/hypergeometric.py
+++ mpsci/stats/distributions/hypergeometric.py
@@ -1,41 +1,68 @@
+"""
+The hypergeometric distribution.
+"""
+
 import mpmath
 
 
-def pmf(k, M, n, N):
-    tot, good = M, n
-    bad = tot - good
-    numer = (mpmath.beta(good + 1, 1) *
-             mpmath.beta(bad + 1, 1) *
-             mpmath.beta(tot - N + 1, N + 1))
-    denom = (mpmath.beta(k + 1, good - k + 1) *
-             mpmath.beta(N - k + 1, bad - N + k + 1) *
-             mpmath.beta(tot + 1, 1))
+def pmf(k, ntotal, ngood, nsample):
+    """
+    Probability mass function of the hypergeometric distribution.
+    """
+    nbad = ntotal - ngood
+    numer = (ntotal + 1) * mpmath.beta(ntotal - nsample + 1, nsample + 1)
+    denom = ((ngood + 1) * (nbad + 1) * mpmath.beta(k + 1, ngood - k + 1) *
+             mpmath.beta(nsample - k + 1, nbad - nsample + k + 1))
     pmf = numer / denom
     return pmf
 
 
-def sf(k, M, n, N):
-    h = mpmath.hyp3f2(1, k + 1 - n, k + 1 - N, k + 2, M + k + 2 - n - N, 1)
-    sf = (mpmath.binomial(N, k + 1) * mpmath.binomial(M - N, n - k - 1) /
-          mpmath.binomial(M, n) * h)
+def cdf(k, ntotal, ngood, nsample):
+    """
+    Cumulative distribution function of the hypergeometric distribution.
+    """
+    return 1 - sf(k, ntotal, ngood, nsample)
+
+
+def sf(k, ntotal, ngood, nsample):
+    """
+    Survival function of the hypergeometric distribution.
+    """
+    h = mpmath.hyp3f2(1, k + 1 - ngood, k + 1 - nsample, k + 2, ntotal + k + 2 - ngood - nsample, 1)
+    sf = (mpmath.binomial(nsample, k + 1) * mpmath.binomial(ntotal - nsample, ngood - k - 1) /
+          mpmath.binomial(ntotal, ngood) * h)
     return sf
 
 
-# XXX Call hypergeometric_pmf instead of duplicating the implementation.
-# XXX Reconcile the API: "M, n, N" vs "good, bad, nsample"
-def support(good, bad, nsample):
-    tot = good + bad
-    pmf = []
-    expected = []
-    support = range(max(0, nsample - bad), min(nsample + 1, good + 1))
+def support(ntotal, ngood, nsample):
+    """
+    Support of the hypergeometric distribution.
+
+    Returns
+    -------
+    sup : range
+        The range of integers in the support.  (In Python 3, use list(sup) to
+        get the list of integers in the support.)
+    p : sequence of mpmath floats
+        The probability of each integer in the support.
+
+    Examples
+    --------
+    >>> sup, pvals = hypergeometric.support(20, 14, 5)
+    >>> for k, p in zip(sup, pvals):
+    ...     print("{:2} {:10.7f}".format(k, float(p)))
+    ... 
+     0  0.0003870
+     1  0.0135449
+     2  0.1173891
+     3  0.3521672
+     4  0.3873839
+     5  0.1291280
+
+    """
+    nbad = ntotal - ngood
+    p = []
+    support = range(max(0, nsample - nbad), min(nsample, ngood) + 1)
     for k in support:
-        # XXX beta(z, 1) can be simplified.
-        numer = (mpmath.beta(good + 1, 1) *
-                 mpmath.beta(bad + 1, 1) *
-                 mpmath.beta(tot - nsample + 1, nsample + 1))
-        denom = (mpmath.beta(k + 1, good - k + 1) *
-                 mpmath.beta(nsample - k + 1, bad - nsample + k + 1) *
-                 mpmath.beta(tot + 1, 1))
-        pk = numer / denom
-        pmf.append(pk)
-    return support, pmf
+        p.append(pmf(k, ntotal, ngood, nsample))
+    return support, p
---
+++ mpsci/stats/distributions/tests/test_hypergeometric.py
@@ -0,0 +1,36 @@
+
+import mpmath
+from mpsci.stats.distributions import hypergeometric
+
+
+def test_basic():
+    with mpmath.workdps(50):
+        ntotal = 20
+        ngood = 14
+        nsample = 5
+        # Precomputed results using Wolfram
+        #   PMF{HypergeometricDistribution[5, 14, 20], k}
+        #   CDF{HypergeometricDistribution[5, 14, 20], k}
+        expected_pmf = [
+            mpmath.mpf('1/2584'),
+            mpmath.mpf('35/2584'),
+            mpmath.mpf('455/3876'),
+            mpmath.mpf('455/1292'),
+            mpmath.mpf('1001/2584'),
+            mpmath.mpf('1001/7752'),
+        ]
+        expected_cdf = [
+            mpmath.mpf('1/2584'),
+            mpmath.mpf('9/646'),
+            mpmath.mpf('509/3876'),
+            mpmath.mpf('937/1938'),
+            mpmath.mpf('6751/7752'),
+            mpmath.mpf('1'),
+        ]
+        for k in range(len(expected_pmf)):
+            p = hypergeometric.pmf(k, ntotal, ngood, nsample)
+            assert mpmath.almosteq(p, expected_pmf[k])
+            c = hypergeometric.cdf(k, ntotal, ngood, nsample)
+            assert mpmath.almosteq(c, expected_cdf[k])
+            s = hypergeometric.sf(k, ntotal, ngood, nsample)
+            assert mpmath.almosteq(s, 1 - expected_cdf[k])
