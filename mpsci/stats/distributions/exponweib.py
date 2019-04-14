55b806298e43c340bc9d03ad986bcccf0bd76600
Warren Weckesser <warren.weckesser@gmail.com>
2019-04-14 18:02:19 -0400

    Updates for the exponentiated Weibull distribution.

--- mpsci/stats/distributions/exponweib.py
+++ mpsci/stats/distributions/exponweib.py
@@ -2,15 +2,66 @@
 import mpmath
 
 
-__all__ = ['pdf']
+__all__ = ['pdf', 'logpdf', 'cdf']
 
 
-def pdf(x, a, c):
+def pdf(x, a, c, scale=1):
     """
     PDF for the exponentiated Weibull distribution.
+
+    All the distribution parameters are assumed to be positive.
     """
     x = mpmath.mpf(x)
     a = mpmath.mpf(a)
     c = mpmath.mpf(c)
-    p = a * c * x**(c-1) * (- mpmath.expm1(-x**c))**(a - 1) * mpmath.exp(-x**c)
+    scale = mpmath.mpf(scale)
+
+    if x < 0:
+        return mpmath.mp.zero
+
+    z = x / scale
+    p = (a * c / scale * z**(c-1) * (-mpmath.expm1(-z**c))**(a - 1) *
+         mpmath.exp(-z**c))
     return p
+
+
+def logpdf(x, a, c, scale=1):
+    """
+    Logarithm of the PDF for the exponentiated Weibull distribution.
+
+    All the distribution parameters are assumed to be positive.
+    """
+    x = mpmath.mpf(x)
+    a = mpmath.mpf(a)
+    c = mpmath.mpf(c)
+    scale = mpmath.mpf(scale)
+
+    if x < 0:
+        return -mpmath.mp.inf
+
+    z = x / scale
+    logp = (mpmath.log(a)
+            + mpmath.log(c)
+            - mpmath.log(scale)
+            + (c - 1)*mpmath.log(z)
+            + (a - 1)*mpmath.log(-mpmath.expm1(-z**c))
+            - z**c)
+    return logp
+
+
+def cdf(x, a, c, scale=1):
+    """
+    Cumulative distribution function for the exponentiated Weibull distribution.
+
+    All the distribution parameters are assumed to be positive.
+    """
+    x = mpmath.mpf(x)
+    a = mpmath.mpf(a)
+    c = mpmath.mpf(c)
+    scale = mpmath.mpf(scale)
+
+    if x < 0:
+        return mpmath.mp.zero
+
+    z = x / scale
+    return mpmath.power(-mpmath.expm1(-z**c), a)
