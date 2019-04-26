d8216abfba1b5db671c4a70c954d482dabfaadeb
Warren Weckesser <warren.weckesser@gmail.com>
2019-04-26 11:58:21 -0400

    Add fun.xlog1py

--- mpsci/fun/_xlogy.py
+++ mpsci/fun/_xlogy.py
@@ -2,7 +2,7 @@
 from mpmath import mp
 
 
-__all__ = ['xlogy']
+__all__ = ['xlogy', 'xlog1py']
 
 
 def xlogy(x, y):
@@ -17,3 +17,20 @@ def xlogy(x, y):
         return mp.zero
     else:
         return x * mp.log(y)
+
+
+def xlog1py(x, y):
+    """
+    x*log(1+y)
+
+    If x is 0 and y is not nan, 0 is returned.
+
+    This function is mathematically equivalent to `xlogy(1 + y)`.  It avoids
+    the loss of precision that can result if y is very small.
+    """
+    if mp.isnan(x) or mp.isnan(y):
+        return mp.nan
+    if x == 0:
+        return mp.zero
+    else:
+        return x * mp.log1p(y)
