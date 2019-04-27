8bf0338d7d3a66b9d09b5cf35c36bd42f4c5d971
Warren Weckesser <warren.weckesser@gmail.com>
2019-04-27 17:47:47 -0400

    Add the formula to the marcumq docstring.

--- mpsci/fun/_marcumq.py
+++ mpsci/fun/_marcumq.py
@@ -1,4 +1,5 @@
 
+import re
 import mpmath
 
 
@@ -14,6 +15,12 @@ def marcumq(m, a, b):
     """
     The Marcum Q function.
 
+    The function is defined as the integral from b to infinity of
+
+        x*(x/a)**(M-1)*exp(-(x**2 + a**2)/2)*I_{M-1}(a*x)
+
+    where I_{M-1}(x) is the modified Bessel function of order M-1.
+
     The function uses numerical integration, so it can be very slow.
     """
     if a == 0:
@@ -27,6 +34,18 @@ def marcumq(m, a, b):
         q = mpmath.quad(lambda x: _integrand(x, m, a), [b, mpmath.inf])
     return q
 
+marcumq._docstring_re_subs = [
+    ('is defined as .*a\*x\)',
+     r'''is defined as
+
+.. math::
+
+   Q_M(a, b) = \\int_b^{\\infty} x \\left(\\frac{x}{a}\\right)^{M-1}
+               \\exp\\left(-\\frac{x^2 + a^2}{2}\\right) I_{M-1}(ax)\\,dx
+''', 0, re.DOTALL),
+    ('I_\{M-1\}\(x\)', r':math:`I_{M-1}(z)`', 0, 0),
+    ('M-1\.', r':math:`M-1`', 0, 0),
+]
 
 def cmarcumq(m, a, b):
     """
