0523fb06b1067dfc74d455203333862a793f5e5e
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-03 00:51:21 -0400

    Add the formula for the beta function to the logbeta docstring.

--- mpsci/fun/_logbeta.py
+++ mpsci/fun/_logbeta.py
@@ -1,4 +1,5 @@
 
+import re
 import mpmath
 
 
@@ -7,9 +8,30 @@ __all__ = ['logbeta']
 
 def logbeta(x, y):
     """
-    Log of beta(x, y)
+    Natural logarithm of beta(x, y).
+
+    The beta function is
+
+                     Gamma(x) Gamma(y)
+        beta(x, y) = -----------------
+                       Gamma(x + y)
+
+    where Gamma(z) is the Gamma function.
     """
     with mpmath.extradps(5):
         mx = mpmath.mpf(x)
         my = mpmath.mpf(y)
         return mpmath.loggamma(x) + mpmath.loggamma(y) - mpmath.loggamma(mx + my)
+
+
+_beta_func_latex = r"""
+    .. math::
+
+        B(x, y) = \\frac{\\Gamma(x)\\Gamma(y)}{\\Gamma(x + y)}
+"""
+
+logbeta._docstring_re_subs = [
+    (r'     *Gamma\(x.*x \+ y\)', _beta_func_latex, 0, re.DOTALL),
+    (' Gamma\(z\)', r' :math:`\\Gamma(z)`', 0, 0),
+    (' beta\(x, y\)', r' :math:`B(x, y)`', 0, 0),
+]
