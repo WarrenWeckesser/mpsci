576f61496eb05e38f65755a58830775b14003664
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-08 20:57:00 -0400

    In the build script, add lines to remove sphinx intermediate files.

--- doc/builddocs.sh
+++ doc/builddocs.sh
@@ -1,4 +1,13 @@
+# Remove all the old files.
 rm -rf source
 rm -rf build
+
+# Generate the .rst files in source/
 python myautogen.py
+
+# Run sphinx to build the html pages.
 sphinx-build -c . -b html -a -E source build
+
+# Remove sphinx intermediate files.
+rm -f build/.buildinfo
+rm -rf build/.doctrees
