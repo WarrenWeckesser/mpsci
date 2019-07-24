07a6bd593427fa69fc0aa556ef19d56bd82e09c0
Warren Weckesser <warren.weckesser@gmail.com>
2019-07-24 12:37:15 -0400

    ENH: Add the function pearsonr_ci to stats.

--- mpsci/stats/__init__.py
+++ mpsci/stats/__init__.py
@@ -9,6 +9,6 @@ in `scipy.stats`.
 
 from ._basic import *
 from ._goftests import *
-from ._pearsonr import pearsonr
+from ._pearsonr import pearsonr, pearsonr_ci
 from ._anova import anova_oneway
 from ._fisher_exact import fisher_exact
--- mpsci/stats/_pearsonr.py
+++ mpsci/stats/_pearsonr.py
@@ -1,6 +1,7 @@
 
 
 import mpmath
+from ..distributions import normal
 
 
 def pearsonr(x, y):
@@ -41,3 +42,54 @@ def pearsonr(x, y):
     p = 2*mpmath.betainc(a, a, x2=0.5*(1-abs(r)))/mpmath.beta(a, a)
 
     return r, p
+
+
+def pearsonr_ci(r, n, alpha):
+    """
+    Confidence interval of Pearson's correlation coefficient.
+
+    This function uses Fisher's transformation to compute the confidence
+    interval of Pearson's correlation coefficient.
+
+    Examples
+    --------
+    Imports:
+
+    >>> import mpmath
+    >>> mpmath.mp.dps = 20
+    >>> from mpsci.stats import pearsonr, pearsonr_ci
+
+    Sample data:
+
+    >>> a = [2, 4, 5, 7, 10, 11, 12, 15, 16, 20]
+    >>> b = [2.53, 2.41, 3.60, 2.69, 3.19, 4.05, 3.71, 4.65, 4.33, 4.70]
+
+    Compute the correlation coefficient:
+
+    >>> r, p = pearsonr(a, b)
+    >>> r
+    mpf('0.893060379514729854846')
+    >>> p
+    mpf('0.00050197523992669206603645')
+
+    Compute the 95% confidence interval for r:
+
+    >>> rlo, rhi = pearsonr_ci(r, n=len(a), alpha=0.05)
+    >>> rlo
+    mpf('0.60185206817708369265664')
+    >>> rhi
+    mpf('0.97464778383702233502275')
+
+    """
+    with mpmath.mp.extradps(5):
+        zr = mpmath.atanh(r)
+        n = mpmath.mp.mpf(n)
+        alpha = mpmath.mp.mpf(alpha)
+        v = 1 / (n - 3)
+        s = mpmath.sqrt(v)
+        h = normal.invcdf(1 - alpha/2)
+        zlo = zr - h*s
+        zhi = zr + h*s
+        rlo = mpmath.tanh(zlo)
+        rhi = mpmath.tanh(zhi)
+        return rlo, rhi
