9407f6215f472c84c31861ea2276647f7bf08fe9
Warren Weckesser <warren.weckesser@gmail.com>
2020-03-14 14:06:13 -0400

    distributions: handle out-of-support values and locally boost precision for gamma_gompertz functions

--- mpsci/distributions/gamma_gompertz.py
+++ mpsci/distributions/gamma_gompertz.py
@@ -26,40 +26,49 @@ def pdf(x, c, beta, scale):
     """
     Probability density function of the Gamma-Gompertz distribution.
     """
-    x = mpmath.mpf(x)
-    beta = mpmath.mpf(beta)
-    c = mpmath.mpf(c)
-    scale = mpmath.mpf(scale)
+    with mpmath.extradps(5):
+        if x < 0:
+            return mpmath.mp.zero
+        x = mpmath.mpf(x)
+        beta = mpmath.mpf(beta)
+        c = mpmath.mpf(c)
+        scale = mpmath.mpf(scale)
 
-    ex = mpmath.exp(x/scale)
-    num = c * ex * mpmath.power(beta, c)
-    den = scale * mpmath.power(beta - 1 + ex, c + 1)
-    return num / den
+        ex = mpmath.exp(x/scale)
+        num = c * ex * mpmath.power(beta, c)
+        den = scale * mpmath.power(beta - 1 + ex, c + 1)
+        return num / den
 
 
 def cdf(x, c, beta, scale):
     """
     Cumulative distribution function of the Gamma-Gompertz distribution.
     """
-    x = mpmath.mpf(x)
-    beta = mpmath.mpf(beta)
-    c = mpmath.mpf(c)
-    scale = mpmath.mpf(scale)
+    with mpmath.extradps(5):
+        if x < 0:
+            return mpmath.mp.zero
+        x = mpmath.mpf(x)
+        beta = mpmath.mpf(beta)
+        c = mpmath.mpf(c)
+        scale = mpmath.mpf(scale)
 
-    ex = mpmath.exp(x/scale)
-    p = -mpmath.powm1(beta / (beta - 1 + ex), c)
-    return p
+        ex = mpmath.exp(x/scale)
+        p = -mpmath.powm1(beta / (beta - 1 + ex), c)
+        return p
 
 
 def sf(x, c, beta, scale):
     """
     Cumulative distribution function of the Gamma-Gompertz distribution.
     """
-    x = mpmath.mpf(x)
-    beta = mpmath.mpf(beta)
-    c = mpmath.mpf(c)
-    scale = mpmath.mpf(scale)
+    with mpmath.extradps(5):
+        if x < 0:
+            return mpmath.mp.one
+        x = mpmath.mpf(x)
+        beta = mpmath.mpf(beta)
+        c = mpmath.mpf(c)
+        scale = mpmath.mpf(scale)
 
-    ex = mpmath.exp(x/scale)
-    p = mpmath.power(beta / (beta - 1 + ex), c)
-    return p
+        ex = mpmath.exp(x/scale)
+        p = mpmath.power(beta / (beta - 1 + ex), c)
+        return p
