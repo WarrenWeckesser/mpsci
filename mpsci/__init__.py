874bb3c6c874fb953c43d23421d159ff988a45c4
Warren Weckesser <warren.weckesser@gmail.com>
2020-08-15 11:23:44 -0400

    Add ncf to the distributions namespace.

--- mpsci/__init__.py
+++ mpsci/__init__.py
@@ -5,7 +5,7 @@ calculations.
 
 """
 
-__version__ = "0.0.1.dev9"
+__version__ = "0.0.1.dev10"
 
 from . import distributions
 from . import fun
--- mpsci/distributions/__init__.py
+++ mpsci/distributions/__init__.py
@@ -12,6 +12,6 @@ as the corresponding implementations in `scipy.stats`.
 from . import (beta, binomial, cosine, exponweib, f,
                fishers_noncentral_hypergeometric, gamma, gamma_gompertz,
                genexpon, genextreme, geninvgauss, genpareto, hypergeometric,
-               laplace, logistic, lognormal, multivariate_hypergeometric, ncx2,
-               negative_binomial, negative_hypergeometric, normal, poisson,
-               rice, t)
+               laplace, logistic, lognormal, multivariate_hypergeometric, ncf,
+               ncx2, negative_binomial, negative_hypergeometric, normal,
+               poisson, rice, t)
