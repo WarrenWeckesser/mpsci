c427ac61a4532ea0b9c0591d807ce30cac079cd3
Warren Weckesser <warren.weckesser@gmail.com>
2020-11-09 01:24:01 -0500

    ENH: Add the noncentral t distribution (only pdf, mean and var)

--- mpsci/distributions/__init__.py
+++ mpsci/distributions/__init__.py
@@ -14,6 +14,6 @@ from . import (argus, benktander1, benktander2, beta, binomial, chi2, cosine,
                gamma_gompertz, genexpon, genextreme, geninvgauss, genpareto,
                gumbel_max, gumbel_min,
                hypergeometric, laplace, levy, logistic, lognormal,
-               multivariate_hypergeometric, multivariate_t, ncf, ncx2,
+               multivariate_hypergeometric, multivariate_t, ncf, nct, ncx2,
                negative_binomial, negative_hypergeometric, normal, poisson,
                rice, t, weibull_max, weibull_min)
---
+++ mpsci/distributions/nct.py
@@ -0,0 +1,65 @@
+"""
+Noncentral t distribution
+-------------------------
+
+Currently only the shape parameters (degrees of freedom `df` and
+noncentrality `nc`) are implemented.
+
+"""
+
+import mpmath
+
+
+__all__ = ['pdf', 'mean', 'var']
+
+
+def pdf(x, df, nc):
+    """
+    Probability density function of the noncentral t distribution.
+
+    The infinite series is estimated with `mpmath.nsum`.
+    """
+    with mpmath.extradps(5):
+        x = mpmath.mpf(x)
+        df = mpmath.mpf(df)
+        nc = mpmath.mpf(nc)
+        logc = (df*mpmath.log(df)/2 - nc**2/2 - mpmath.loggamma(df/2)
+                - mpmath.log(mpmath.pi)/2 - (df + 1)/2 * mpmath.log(df + x**2))
+        c = mpmath.exp(logc)
+
+        def _pdf_term(i):
+            logterm = (mpmath.loggamma((df + i + 1)/2)
+                       + i*mpmath.log(x*nc)
+                       + i*mpmath.log(2/(df + x**2))/2
+                       - mpmath.loggamma(i + 1))
+            return mpmath.exp(logterm)
+
+        s = mpmath.nsum(_pdf_term, [0, mpmath.inf])
+        return c * s
+
+
+def mean(df, nc):
+    """
+    Mean of the noncentral t distribution.
+    """
+    # XXX Require df > 1.
+    with mpmath.extradps(5):
+        df = mpmath.mpf(df)
+        nc = mpmath.mpf(nc)
+        logm = (mpmath.log(nc)
+                + mpmath.log(df/2)/2
+                + mpmath.loggamma((df - 1)/2)
+                - mpmath.loggamma(df/2))
+        return mpmath.exp(logm)
+
+
+def var(df, nc):
+    """
+    Variance of the noncentral t distribution.
+    """
+    # XXX Require df > 2.
+    with mpmath.extradps(5):
+        df = mpmath.mpf(df)
+        nc = mpmath.mpf(nc)
+        c = mpmath.exp(mpmath.loggamma((df - 1)/2) - mpmath.loggamma(df/2))
+        return df/(df - 2) * (1 + nc**2) - df/2 * nc**2 * c**2
