c289e40a46a52b03806baf5e36807ce8ac54e81f
Warren Weckesser <warren.weckesser@gmail.com>
2020-12-27 17:16:45 -0500

    MAINT: Add interval_prob to __all__ in beta.py

--- mpsci/distributions/beta.py
+++ mpsci/distributions/beta.py
@@ -8,8 +8,8 @@ from .. import fun as _fun
 from ..stats import mean as _mean, var as _var
 
 
-__all__ = ['pdf', 'logpdf', 'cdf', 'invcdf', 'sf', 'invsf', 'mean', 'mle',
-           'mom']
+__all__ = ['pdf', 'logpdf', 'cdf', 'invcdf', 'sf', 'invsf', 'interval_prob',
+           'mean', 'mle', 'mom']
 
 
 def pdf(x, a, b):
