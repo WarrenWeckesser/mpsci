5ca83e25ece7571ddcfde1cdf156a1b664be04f7
warren <warren.weckesser@gmail.com>
2022-01-04 13:59:28 -0500

    MAINT: Don't use 'import *' in fun.__init__.py

--- mpsci/fun/__init__.py
+++ mpsci/fun/__init__.py
@@ -6,12 +6,12 @@ This module contains an assortment of functions that arise
 in scientific computing.
 """
 
-from ._boxcox import *
+from ._boxcox import boxcox, boxcox1p
 from ._yeo_johnson import yeo_johnson, inv_yeo_johnson
-from ._marcumq import *
-from ._digammainv import *
-from ._xlogy import *
-from ._logbeta import *
-from ._logbinomial import *
-from ._logsumexp import *
+from ._marcumq import marcumq, cmarcumq
+from ._digammainv import digammainv
+from ._xlogy import xlogy, xlog1py
+from ._logbeta import logbeta
+from ._logbinomial import logbomial
+from ._logsumexp import logsumexp
 from ._legendre import roots_legendre
