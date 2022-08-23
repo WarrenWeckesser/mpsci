60e04cbfe9c3073bb11e33f54ba905077f7687f2
warren <warren.weckesser@gmail.com>
2022-08-23 08:27:26 -0400

    Remove 'linspace'--it is in mpmath.

--- mpsci/__init__.py
+++ mpsci/__init__.py
@@ -5,7 +5,7 @@ calculations.
 
 """
 
-__version__ = "0.0.1.dev37"
+__version__ = "0.0.1.dev38"
 
 from . import distributions
 from . import fun
--- mpsci/fun/__init__.py
+++ mpsci/fun/__init__.py
@@ -15,5 +15,4 @@ from ._logbeta import logbeta
 from ._logbinomial import logbinomial
 from ._logsumexp import logsumexp
 from ._legendre import roots_legendre
-from ._linspace import linspace
 from ._powm1 import inv_powm1, pow1pm1, inv_pow1pm1
