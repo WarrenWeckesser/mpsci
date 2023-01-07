621bf4ef3ebdf92ffa2e4023d5e2683e54a93ad3
warren <warren.weckesser@gmail.com>
2023-01-07 08:41:15 -0500

    MAINT: Update chi.py: use mp namespace; return value from _validate_k

--- mpsci/distributions/chi.py
+++ mpsci/distributions/chi.py
@@ -3,7 +3,7 @@ Chi distribution
 ----------------
 """
 
-import mpmath
+from mpmath import mp
 
 
 __all__ = ['pdf', 'logpdf', 'cdf', 'sf', 'mode', 'mean', 'variance']
@@ -12,30 +12,31 @@ __all__ = ['pdf', 'logpdf', 'cdf', 'sf', 'mode', 'mean', 'variance']
 def _validate_k(k):
     if k <= 0:
         raise ValueError('k must be positive')
+    return mp.mpf(k)
 
 
 def pdf(x, k):
     """
     PDF for the chi distribution.
     """
-    _validate_k(k)
-    if x < 0:
-        return mpmath.mp.zero
-    return mpmath.exp(logpdf(x, k))
+    with mp.extradps(5):
+        k = _validate_k(k)
+        if x < 0:
+            return mp.zero
+        return mp.exp(logpdf(x, k))
 
 
 def logpdf(x, k):
     """
     Logarithm of the PDF for the chi distribution.
     """
-    _validate_k(k)
-    if x < 0:
-        return mpmath.mp.ninf
-    with mpmath.extradps(5):
-        x = mpmath.mpf(x)
-        k = mpmath.mpf(k)
-        p = ((k - 1)*mpmath.log(x) - x**2/2 - ((k/2) - 1)*mpmath.log(2)
-             - mpmath.loggamma(k/2))
+    with mp.extradps(5):
+        k = _validate_k(k)
+        x = mp.mpf(x)
+        if x < 0:
+            return mp.ninf
+        p = ((k - 1)*mp.log(x) - x**2/2 - ((k/2) - 1)*mp.log(2)
+             - mp.loggamma(k/2))
         return p
 
 
@@ -43,27 +44,26 @@ def cdf(x, k):
     """
     CDF for the chi distribution.
     """
-    _validate_k(k)
-    if x <= 0:
-        return mpmath.mp.zero
-    with mpmath.extradps(5):
-        x = mpmath.mpf(x)
-        k = mpmath.mpf(k)
-        c = mpmath.gammainc(k/2, a=0, b=x**2/2, regularized=True)
-    return c
+    with mp.extradps(5):
+        k = _validate_k(k)
+        x = mp.mpf(x)
+        if x <= 0:
+            return mp.zero
+        c = mp.gammainc(k/2, a=0, b=x**2/2, regularized=True)
+        return c
 
 
 def sf(x, k):
     """
     Survival function for the chi distribution.
     """
-    _validate_k(k)
-    if x <= 0:
-        return mpmath.mp.one
-    with mpmath.extradps(5):
-        x = mpmath.mpf(x)
-        k = mpmath.mpf(k)
-        s = mpmath.gammainc(k/2, a=x**2/2, b=mpmath.inf, regularized=True)
+
+    with mp.extradps(5):
+        k = _validate_k(k)
+        x = mp.mpf(x)
+        if x <= 0:
+            return mp.one
+        s = mp.gammainc(k/2, a=x**2/2, b=mp.inf, regularized=True)
     return s
 
 
@@ -75,11 +75,11 @@ def mode(k):
 
     For 0 < k < 1, 0 is returned.
     """
-    _validate_k(k)
-    with mpmath.extradps(5):
+    with mp.extradps(5):
+        k = _validate_k(k)
         if k < 1:
-            return mpmath.mp.zero
-        return mpmath.sqrt(k - 1)
+            return mp.zero
+        return mp.sqrt(k - 1)
 
 
 mode._docstring_re_subs = ([
@@ -93,19 +93,17 @@ def mean(k):
     """
     Mean of the chi distribution.
     """
-    _validate_k(k)
-    with mpmath.extradps(5):
-        k = mpmath.mpf(k)
-        return mpmath.sqrt(2) * mpmath.gamma((k + 1)/2) / mpmath.gamma(k/2)
-    return k
+    with mp.extradps(5):
+        k = _validate_k(k)
+        return mp.sqrt(2) * mp.gamma((k + 1)/2) / mp.gamma(k/2)
+        return k
 
 
 def variance(k):
     """
     Variance of the chi distribution.
     """
-    _validate_k(k)
-    with mpmath.extradps(5):
-        k = mpmath.mpf(k)
+    with mp.extradps(5):
+        k = _validate_k(k)
         mu = mean(k)
         return k - mu**2
