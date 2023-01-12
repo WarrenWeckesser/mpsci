bc8f3b24bb03e5745d350396c427d4ae9638a644
warren <warren.weckesser@gmail.com>
2023-01-12 08:24:37 -0500

    MAINT: Use mp namespace in normal.py

--- mpsci/distributions/normal.py
+++ mpsci/distributions/normal.py
@@ -3,7 +3,7 @@ Normal distribution
 -------------------
 """
 
-import mpmath
+from mpmath import mp
 from ._common import _validate_p
 
 
@@ -14,19 +14,19 @@ def pdf(x, mu=0, sigma=1):
     """
     Normal distribution probability density function.
     """
-    # Defined here for consistency, but this is just mpmath.npdf
-    return mpmath.npdf(x, mu, sigma)
+    # Defined here for consistency, but this is just mp.npdf
+    return mp.npdf(x, mu, sigma)
 
 
 def logpdf(x, mu=0, sigma=1):
     """
     Logarithm of the PDF of the normal distribution.
     """
-    with mpmath.extradps(5):
-        x = mpmath.mpf(x)
-        mu = mpmath.mpf(mu)
-        sigma = mpmath.mpf(sigma)
-        logp = (-mpmath.log(2*mpmath.pi)/2 - mpmath.log(sigma)
+    with mp.extradps(5):
+        x = mp.mpf(x)
+        mu = mp.mpf(mu)
+        sigma = mp.mpf(sigma)
+        logp = (-mp.log(2*mp.pi)/2 - mp.log(sigma)
                 - (x - mu)**2/(2*sigma**2))
     return logp
 
@@ -35,15 +35,19 @@ def cdf(x, mu=0, sigma=1):
     """
     Normal distribution cumulative distribution function.
     """
-    # Defined here for consistency, but this is just mpmath.ncdf
-    return mpmath.ncdf(x, mu, sigma)
+    # Defined here for consistency, but this is just mp.ncdf
+    return mp.ncdf(x, mu, sigma)
 
 
 def sf(x, mu=0, sigma=1):
     """
     Normal distribution survival function.
     """
-    return mpmath.ncdf(-x + 2*mu, mu, sigma)
+    with mp.extradps(5):
+        x = mp.mpf(x)
+        mu = mp.mpf(mu)
+        sigma = mp.mpf(sigma)
+        return mp.ncdf(-x + 2*mu, mu, sigma)
 
 
 def invcdf(p, mu=0, sigma=1):
@@ -53,13 +57,13 @@ def invcdf(p, mu=0, sigma=1):
     This function is also known as the quantile function or the percent
     point function.
     """
-    with mpmath.extradps(mpmath.mp.dps):
+    with mp.extradps(mp.dps):
         p = _validate_p(p)
-        mu = mpmath.mpf(mu)
-        sigma = mpmath.mpf(sigma)
+        mu = mp.mpf(mu)
+        sigma = mp.mpf(sigma)
 
-        a = mpmath.erfinv(2*p - 1)
-        x = mpmath.sqrt(2)*sigma*a + mu
+        a = mp.erfinv(2*p - 1)
+        x = mp.sqrt(2)*sigma*a + mu
         return x
 
 
@@ -67,13 +71,13 @@ def invsf(p, mu=0, sigma=1):
     """
     Inverse of the survival function of the normal distribution.
     """
-    with mpmath.extradps(mpmath.mp.dps):
+    with mp.extradps(mp.dps):
         p = _validate_p(p)
-        mu = mpmath.mpf(mu)
-        sigma = mpmath.mpf(sigma)
+        mu = mp.mpf(mu)
+        sigma = mp.mpf(sigma)
 
-        a = mpmath.erfinv(1 - 2*p)
-        x = mpmath.sqrt(2)*sigma*a + mu
+        a = mp.erfinv(1 - 2*p)
+        x = mp.sqrt(2)*sigma*a + mu
         return x
 
 
@@ -85,9 +89,9 @@ def mle(x):
 
     Returns (mu, sigma).
     """
-    x = [mpmath.mpf(t) for t in x]
+    x = [mp.mpf(t) for t in x]
     N = len(x)
     meanx = sum(x) / N
     var = sum((xi - meanx)**2 for xi in x) / N
-    sigma = mpmath.sqrt(var)
+    sigma = mp.sqrt(var)
     return meanx, sigma
