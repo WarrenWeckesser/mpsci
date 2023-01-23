29eac49028bbe9baf6b4e5b77bcf18925113a3fa
warren <warren.weckesser@gmail.com>
2023-01-22 23:03:32 -0500

    BUG: Fix invgauss.pdf and invgauss.logpdf.

--- mpsci/distributions/invgauss.py
+++ mpsci/distributions/invgauss.py
@@ -8,7 +8,7 @@ implementation in `scipy.stats.invgauss`.  `mu` is a shape parameter;
 
 """
 
-import mpmath
+from mpmath import mp
 from ._common import _validate_p
 
 
@@ -23,9 +23,9 @@ def _validate_params(mu, loc, scale):
         raise ValueError('mu must be positive')
     if scale <= 0:
         raise ValueError('scale must be positive')
-    mu = mpmath.mpf(mu)
-    loc = mpmath.mpf(loc)
-    scale = mpmath.mpf(scale)
+    mu = mp.mpf(mu)
+    loc = mp.mpf(loc)
+    scale = mp.mpf(scale)
     return mu, loc, scale
 
 
@@ -33,15 +33,15 @@ def pdf(x, mu, loc=0, scale=1):
     """
     PDF for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return mpmath.mp.zero
+            return mp.zero
         z = (x - loc)/scale
-        den = mpmath.sqrt(2*mpmath.pi*z**3)
+        den = scale*mp.sqrt(2*mp.pi*z**3)
         t = ((z - mu)/mu)**2
-        num = mpmath.exp(-t/(2*z))
+        num = mp.exp(-t/(2*z))
         return num/den
 
 
@@ -49,15 +49,15 @@ def logpdf(x, mu, loc=0, scale=1):
     """
     Logarithm of the PDF for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return mpmath.ninf
+            return mp.ninf
         z = (x - loc)/scale
         t = ((z - mu)/mu)**2
-        logp = (-0.5*mpmath.log(2*mpmath.pi) - 1.5*mpmath.log(z)
-                - t/(2*z))
+        logp = (-0.5*mp.log(2*mp.pi) - 1.5*mp.log(z)
+                - t/(2*z) - mp.log(scale))
         return logp
 
 
@@ -65,14 +65,14 @@ def cdf(x, mu, loc=0, scale=1):
     """
     CDF for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return mpmath.mp.zero
+            return mp.zero
         z = (x - loc)/scale
-        t1 = mpmath.ncdf((z/mu - 1)/mpmath.sqrt(z))
-        t2 = mpmath.exp(2/mu)*mpmath.ncdf(-(z/mu + 1)/mpmath.sqrt(z))
+        t1 = mp.ncdf((z/mu - 1)/mp.sqrt(z))
+        t2 = mp.exp(2/mu)*mp.ncdf(-(z/mu + 1)/mp.sqrt(z))
         return t1 + t2
 
 
@@ -80,33 +80,33 @@ def logcdf(x, mu, loc=0, scale=1):
     """
     Logarithm of the CDF for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return -mpmath.mp.inf
+            return mp.ninf
         z = (x - loc)/scale
-        t1 = mpmath.log(mpmath.ncdf((z/mu - 1)/mpmath.sqrt(z)))
-        t2 = (2/mu) + mpmath.log(mpmath.ncdf(-(z/mu + 1)/mpmath.sqrt(z)))
-        return t1 + mpmath.log1p(mpmath.exp(t2 - t1))
+        t1 = mp.log(mp.ncdf((z/mu - 1)/mp.sqrt(z)))
+        t2 = (2/mu) + mp.log(mp.ncdf(-(z/mu + 1)/mp.sqrt(z)))
+        return t1 + mp.log1p(mp.exp(t2 - t1))
 
 
 def invcdf(p, mu, loc=0, scale=1):
     """
     Inverse of the CDF for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
         p = _validate_p(p)
         if p == 0:
             return loc
         if p == 1:
-            return mpmath.inf
+            return mp.inf
         x0 = mode(mu, loc, scale)
         # FIXME: This loop assumes convergence!
         while True:
             x1 = x0 + (p - cdf(x0, mu, loc, scale))/pdf(x0, mu, loc, scale)
-            if mpmath.almosteq(x1, x0):
+            if mp.almosteq(x1, x0):
                 break
             x0 = x1
         return x1
@@ -116,14 +116,14 @@ def sf(x, mu, loc=0, scale=1):
     """
     Survival function for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return mpmath.mp.one
+            return mp.one
         z = (x - loc)/scale
-        t1 = mpmath.ncdf(-(z/mu - 1)/mpmath.sqrt(z))
-        t2 = mpmath.exp(2/mu)*mpmath.ncdf(-(z/mu + 1)/mpmath.sqrt(z))
+        t1 = mp.ncdf(-(z/mu - 1)/mp.sqrt(z))
+        t2 = mp.exp(2/mu)*mp.ncdf(-(z/mu + 1)/mp.sqrt(z))
         return t1 - t2
 
 
@@ -131,33 +131,33 @@ def logsf(x, mu, loc=0, scale=1):
     """
     Logarithm of the survival function for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
-        x = mpmath.mpf(x)
+        x = mp.mpf(x)
         if x <= loc:
-            return mpmath.mp.zero
+            return mp.zero
         z = (x - loc)/scale
-        t1 = mpmath.log(mpmath.ncdf(-(z/mu - 1)/mpmath.sqrt(z)))
-        t2 = 2/mu + mpmath.log(mpmath.ncdf(-(z/mu + 1)/mpmath.sqrt(z)))
-        return t1 + mpmath.log1p(-mpmath.exp(t2 - t1))
+        t1 = mp.log(mp.ncdf(-(z/mu - 1)/mp.sqrt(z)))
+        t2 = 2/mu + mp.log(mp.ncdf(-(z/mu + 1)/mp.sqrt(z)))
+        return t1 + mp.log1p(-mp.exp(t2 - t1))
 
 
 def invsf(p, mu, loc=0, scale=1):
     """
     Inverse of the survival function for the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
         p = _validate_p(p)
         if p == 0:
-            return mpmath.inf
+            return mp.inf
         if p == 1:
             return loc
         x0 = mode(mu, loc, scale)
         # FIXME: This loop assumes convergence!
         while True:
             x1 = x0 - (p - sf(x0, mu, loc, scale))/pdf(x0, mu, loc, scale)
-            if mpmath.almosteq(x1, x0):
+            if mp.almosteq(x1, x0):
                 break
             x0 = x1
         return x1
@@ -167,7 +167,7 @@ def mean(mu, loc=0, scale=1):
     """
     Mean of the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
         return scale*mu + loc
 
@@ -176,11 +176,11 @@ def mode(mu, loc=0, scale=1):
     """
     Mode of the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
         s = 3*mu/2
         # t is equivalent to sqrt(1 + 1/s**2) - 1.
-        t = mpmath.expm1(mpmath.log1p(1/s**2)/2)
+        t = mp.expm1(mp.log1p(1/s**2)/2)
         # m = mu*(sqrt(1 + s**2) - s) = mu*s*(sqrt(1 + 1/s**2) - 1) = mu*s*t
         m = mu*s*t
         return scale*m + loc
@@ -190,6 +190,6 @@ def variance(mu, loc=0, scale=1):
     """
     Variance of the inverse Gaussian distribution.
     """
-    with mpmath.extradps(5):
+    with mp.extradps(5):
         mu, loc, scale = _validate_params(mu, loc, scale)
         return mu**3*scale**2
