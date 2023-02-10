a953ad6c89421cd827246e654e7c430d55aa6d8d
warren <warren.weckesser@gmail.com>
2023-02-09 21:14:43 -0500

    MAINT: For nakagami: use mp namespace; return values from _validate_params()

--- mpsci/distributions/nakagami.py
+++ mpsci/distributions/nakagami.py
@@ -18,7 +18,7 @@ infinity as x approaches 0.
 
 """
 
-import mpmath
+from mpmath import mp
 from mpsci import stats
 
 
@@ -26,98 +26,76 @@ __all__ = ['pdf', 'logpdf', 'cdf', 'sf',
            'mean', 'var', 'nll', 'mle']
 
 
-def _validate_params(nu, loc, scale):
+def _validate_params(nu, loc=0, scale=1):
     if nu <= 0:
         raise ValueError('nu must be positive')
     if scale <= 0:
         raise ValueError('sigma must be positive')
+    return mp.mpf(nu), mp.mpf(loc), mp.mpf(scale)
 
 
 def pdf(x, nu, loc=0, scale=1):
     """
     Probability density function for the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    with mpmath.extradps(5):
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
         if x <= loc:
-            return mpmath.mp.zero
-        x = mpmath.mpf(x)
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+            return mp.zero
+        x = mp.mpf(x)
         z = (x - loc)/scale
-        return (2*nu**nu * z**(2*nu - 1) * mpmath.exp(-nu*z**2)
-                / mpmath.gamma(nu) / scale)
+        return (2*nu**nu * z**(2*nu - 1) * mp.exp(-nu*z**2)
+                / mp.gamma(nu) / scale)
 
 
 def logpdf(x, nu, loc=0, scale=1):
     """
     Natural logarithm of the PDF of the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    if x <= 0:
-        return -mpmath.inf
-    with mpmath.extradps(5):
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
         if x <= loc:
-            return mpmath.mp.zero
-        x = mpmath.mpf(x)
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+            return mp.ninf
+        x = mp.mpf(x)
         z = (x - loc)/scale
-        return (mpmath.log(2) + nu*mpmath.log(nu) - mpmath.loggamma(nu)
-                + (2*nu-1)*mpmath.log(z) - nu*z**2 - mpmath.log(scale))
+        return (mp.log(2) + nu*mp.log(nu) - mp.loggamma(nu)
+                + (2*nu-1)*mp.log(z) - nu*z**2 - mp.log(scale))
 
 
 def cdf(x, nu, loc=0, scale=1):
     """
     Cumulative distribution function for the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    if x <= 0:
-        return mpmath.mp.zero
-
-    with mpmath.extradps(5):
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
         if x <= loc:
-            return mpmath.mp.zero
-        x = mpmath.mpf(x)
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+            return mp.zero
+        x = mp.mpf(x)
         z = (x - loc)/scale
-        return mpmath.gammainc(nu, 0, nu*z**2, regularized=True)
+        return mp.gammainc(nu, 0, nu*z**2, regularized=True)
 
 
 def sf(x, nu, loc=0, scale=1):
     """
     Survival function for the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    if x <= 0:
-        return mpmath.mp.one
-
-    with mpmath.extradps(5):
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
         if x <= loc:
-            return mpmath.mp.zero
-        x = mpmath.mpf(x)
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+            return mp.one
+        x = mp.mpf(x)
         z = (x - loc)/scale
-        return mpmath.gammainc(nu, nu*z**2, mpmath.inf, regularized=True)
+        return mp.gammainc(nu, nu*z**2, mp.inf, regularized=True)
 
 
 def mean(nu, loc=0, scale=1):
     """
     Mean of the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    with mpmath.extradps(5):
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
-        gratio = mpmath.gammaprod([nu + 0.5], [nu])
-        mean0 = gratio / mpmath.sqrt(nu)
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
+        gratio = mp.gammaprod([nu + 0.5], [nu])
+        mean0 = gratio / mp.sqrt(nu)
         return loc + scale*mean0
 
 
@@ -125,11 +103,9 @@ def var(nu, loc=0, scale=1):
     """
     Variance of the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    with mpmath.extradps(5):
-        nu = mpmath.mpf(nu)
-        scale = mpmath.mpf(scale)
-        gratio = mpmath.gammaprod([nu + 0.5], [nu])
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
+        gratio = mp.gammaprod([nu + 0.5], [nu])
         var0 = 1 - gratio**2/nu
         return scale**2 * var0
 
@@ -143,18 +119,15 @@ def nll(x, nu, loc, scale):
     """
     Negative log-likelihood function for the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    _validate_x(x, loc=loc)
     n = len(x)
-    with mpmath.extradps(5):
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
+        _validate_x(x, loc=loc)
         z = [(t - loc)/scale for t in x]
-        logsum = mpmath.fsum([mpmath.log(t) for t in z])
-        sqsum = mpmath.fsum([t**2 for t in z])
-        ll = n*(mpmath.log(2) + nu*mpmath.log(nu) - mpmath.loggamma(nu)
-                - mpmath.log(scale)) + (2*nu - 1)*logsum - nu*sqsum
+        logsum = mp.fsum([mp.log(t) for t in z])
+        sqsum = mp.fsum([t**2 for t in z])
+        ll = n*(mp.log(2) + nu*mp.log(nu) - mp.loggamma(nu)
+                - mp.log(scale)) + (2*nu - 1)*logsum - nu*sqsum
         return -ll
 
 
@@ -162,28 +135,25 @@ def nll_grad(x, nu, loc, scale):
     """
     Gradient of the negative log-likelihood for the Nakagami distribution.
     """
-    _validate_params(nu, loc, scale)
-    _validate_x(x, loc=loc)
     n = len(x)
-    with mpmath.extradps(5):
-        nu = mpmath.mpf(nu)
-        loc = mpmath.mpf(loc)
-        scale = mpmath.mpf(scale)
+    with mp.extradps(5):
+        nu, loc, scale = _validate_params(nu, loc, scale)
+        _validate_x(x, loc=loc)
         xloc = [(t - loc) for t in x]
-        dldnu = (n*(1 + mpmath.log(nu) - mpmath.digamma(nu))
-                 + 2*mpmath.fsum([mpmath.log(t/scale) for t in xloc])
-                 - mpmath.fsum([t**2 for t in xloc])/scale**2)
-        sum_inv = mpmath.fsum([1/t for t in xloc])
-        dldloc = -(2*nu - 1)*sum_inv + 2*nu*mpmath.fsum(xloc)/scale**2
-        sum_sq = mpmath.fsum([t**2 for t in xloc])
+        dldnu = (n*(1 + mp.log(nu) - mp.digamma(nu))
+                 + 2*mp.fsum([mp.log(t/scale) for t in xloc])
+                 - mp.fsum([t**2 for t in xloc])/scale**2)
+        sum_inv = mp.fsum([1/t for t in xloc])
+        dldloc = -(2*nu - 1)*sum_inv + 2*nu*mp.fsum(xloc)/scale**2
+        sum_sq = mp.fsum([t**2 for t in xloc])
         dldscale = (2*nu/scale)*(-n + sum_sq/scale**2)
         return dldnu, dldloc, dldscale
 
 
 def _mle_nu_func(nu, scale, R):
     # This function is used in mle() to solve log(nu) - digamma(nu) = R.
-    nu = mpmath.mpf(nu)
-    return mpmath.log(nu) - mpmath.digamma(nu) - R
+    nu = mp.mpf(nu)
+    return mp.log(nu) - mp.digamma(nu) - R
 
 
 def _estimate_nu(R):
@@ -215,8 +185,8 @@ def mle(x, nu=None, loc=None, scale=None):
     if loc is not None:
         # loc is fixed; handle this by subtracting loc from x.
         _validate_x(x, loc)
-        with mpmath.extradps(5):
-            loc0 = mpmath.mpf(loc)
+        with mp.extradps(5):
+            loc0 = mp.mpf(loc)
             x = [t - loc0 for t in x]
     else:
         raise ValueError('Fitting `loc` is not implemented yet. '
@@ -226,19 +196,19 @@ def mle(x, nu=None, loc=None, scale=None):
     # Either nu or scale (or both) are not fixed.
 
     if scale is None:
-        scale0 = mpmath.sqrt(mpmath.fsum([t**2 for t in x])/len(x))
+        scale0 = mp.sqrt(mp.fsum([t**2 for t in x])/len(x))
     else:
-        scale0 = mpmath.mpf(scale)
+        scale0 = mp.mpf(scale)
 
     if nu is not None:
-        nu0 = mpmath.mpf(nu)
+        nu0 = mp.mpf(nu)
     else:
         if scale is None:
-            R = -stats.mean([2*mpmath.log(t/scale0) for t in x])
+            R = -stats.mean([2*mp.log(t/scale0) for t in x])
         else:
             R = (stats.mean([(t/scale0)**2 for t in x]) -
-                 stats.mean([2*mpmath.log(t/scale0) for t in x]) - 1)
+                 stats.mean([2*mp.log(t/scale0) for t in x]) - 1)
         nu0 = _estimate_nu(R)
-        nu0 = mpmath.findroot(lambda nu: _mle_nu_func(nu, scale0, R), nu0)
+        nu0 = mp.findroot(lambda nu: _mle_nu_func(nu, scale0, R), nu0)
 
     return nu0, loc0, scale0
