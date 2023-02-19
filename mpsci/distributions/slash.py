2499ad633fab3dfe48e27cdae4d0af1b4006375b
warren <warren.weckesser@gmail.com>
2023-02-19 17:21:35 -0500

    DOC: Add reference for the slash distribution.

--- mpsci/distributions/slash.py
+++ mpsci/distributions/slash.py
@@ -4,6 +4,12 @@ Slash distribution
 
 See https://en.wikipedia.org/wiki/Slash_distribution for details.
 
+According to the wikipedia article, the distribution was named in [1]_.
+
+.. [1] Rogers, W. H.; Tukey, J. W. (1972). "Understanding some long-tailed
+       symmetrical distributions". Statistica Neerlandica. 26 (3): 211–226.
+       doi:10.1111/j.1467-9574.1972.tb00191.x.
+
 """
 
 from mpmath import mp
