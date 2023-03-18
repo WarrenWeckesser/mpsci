3d2053f43ec61506a3a5a43771397b6d48b82bd3
warren <warren.weckesser@gmail.com>
2023-03-18 14:40:45 -0400

    Add a couple more unit tests for genpareto.

--- mpsci/distributions/tests/test_genpareto.py
+++ mpsci/distributions/tests/test_genpareto.py
@@ -4,6 +4,31 @@ from mpmath import mp
 from mpsci.distributions import genpareto
 
 
+@mp.workdps(50)
+def test_pdf():
+    xi = 3
+    x = 5
+    p = genpareto.pdf(x, xi)
+    # The formula for the expected value as given by Mathematica:
+    #    PDF[ParetoPickandsDistribution[3], 5]
+    expected = mp.power(2, -mp.one/3)/32
+    assert mp.almosteq(p, expected)
+
+
+@mp.workdps(50)
+def test_cdf_sf():
+    # The formula for the expected values was given by Mathematica:
+    #     CDF[ParetoPickandsDistribution[3], 5]
+    xi = 3
+    x = 5
+    sf = genpareto.sf(x, xi)
+    expected_sf = mp.power(2, -mp.one/3)/2
+    assert mp.almosteq(sf, expected_sf)
+
+    cdf = genpareto.cdf(x, xi)
+    assert mp.almosteq(cdf, 1 - expected_sf)
+
+
 @pytest.mark.parametrize('func, invfunc',
                          [(genpareto.cdf, genpareto.invcdf),
                           (genpareto.sf, genpareto.invsf)])
@@ -12,6 +37,7 @@ from mpsci.distributions import genpareto
                           (3, 0.5, 2.5, 10.0),
                           (3, 0, 2, 3.0),
                           (3, -2, 1.5, 5.0)])
+@mp.workdps(50)
 def test_cdf_invcdf_sf_invsf_roundtrip(func, invfunc, x, xi, mu, sigma):
     p = func(x, xi, mu, sigma)
     x1 = invfunc(p, xi, mu, sigma)
