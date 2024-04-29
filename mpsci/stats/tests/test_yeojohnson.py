1e9464e31228075546c3f4e0a7a18631683806bb
Warren Weckesser <warren.weckesser@gmail.com>
2024-04-29 14:48:06 -0400

    MAINT: Parametrize a test (clean up an old 'XXX')

--- mpsci/stats/tests/test_yeojohnson.py
+++ mpsci/stats/tests/test_yeojohnson.py
@@ -16,21 +16,12 @@ def test_yeojohnson_equality(x, lmb, expected):
     assert mp.almosteq(yinv, x)
 
 
+@pytest.mark.parametrize('x, lmb, expected_str',
+                         [(3, 0, 'mp.log(4)'),
+                          (-3, 2, '-mp.log(4)')])
 @mp.workdps(60)
-def test_yeojohnson_cases():
-    # XXX To do: convert this to a parametrized test.
-
-    x = 3
-    lmb = 0
-    expected = mp.log(4)
-    y = yeojohnson(x, lmb)
-    assert mp.almosteq(y, expected)
-    yinv = inv_yeojohnson(expected, lmb)
-    assert mp.almosteq(yinv, x)
-
-    x = -3
-    lmb = 2
-    expected = -mp.log(4)
+def test_yeojohnson_cases(x, lmb, expected_str):
+    expected = eval(expected_str)
     y = yeojohnson(x, lmb)
     assert mp.almosteq(y, expected)
     yinv = inv_yeojohnson(expected, lmb)
