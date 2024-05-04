0a65545ff268724997d2d9681d51fe34d3312028
warren <warren.weckesser@gmail.com>
2024-05-04 18:02:05 -0400

    MAINT: Add a basic test for pearsonr.

---
+++ mpsci/stats/tests/test_pearsonr.py
@@ -0,0 +1,14 @@
+from mpmath import mp
+from mpsci.stats import pearsonr
+
+
+def test_pearsonr():
+    x = [1, 2, 3, 5]
+    y = [2, 2, 4, 7]
+    r, p = pearsonr(x, y)
+    # The reference values were computed with Wolfram Alpha:
+    #    Correlation[(1, 2, 3, 5), (2, 2, 4, 7)]
+    # Alpha reports only 6 digits, and does not provide the usual
+    # "more digits" option that other Alpha computations provide.
+    assert mp.almosteq(r, 0.970569, rel_eps=5e-6, abs_eps=0)
+    assert mp.almosteq(p, 0.029431, rel_eps=5e-5, abs_eps=0)
