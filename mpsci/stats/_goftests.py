c8aad2bd22a86baca22592c4f94821139a30c8a4
Warren Weckesser <warren.weckesser@gmail.com>
2024-07-24 21:47:50 -0400

    stats: Require sum(observed) approx equal sum(expected) in chisquare and gtest.

--- mpsci/stats/_goftests.py
+++ mpsci/stats/_goftests.py
@@ -5,20 +5,27 @@ from ..fun import xlogy
 __all__ = ['chisquare', 'gtest']
 
 
-def chisquare(observed, expected):
+def chisquare(observed, expected, sum_rel_tol=None):
     """
     Pearson's chi-square test.
 
     Test whether the observed frequency distribution differs from the
     expected frequency distribution.
     """
+    if sum_rel_tol is None:
+        sum_rel_tol = 10*mp.eps
+    observed = [mp.mpf(t) for t in observed]
+    expected = [mp.mpf(t) for t in expected]
+    if not mp.almosteq(sum(observed), sum(expected),
+                       rel_eps=sum_rel_tol, abs_eps=0):
+        raise ValueError('sum(observed) differs from sum(expected)')
     chi2 = sum((obs - exp)**2/exp for obs, exp in zip(observed, expected))
     df = len(observed) - 1
     p = mp.gammainc(df/2, chi2/2) / mp.gamma(df/2)
     return chi2, p
 
 
-def gtest(observed, expected):
+def gtest(observed, expected, sum_rel_tol=None):
     r"""
     G-test (likelihood-ratio test, maximum likelihood test).
 
@@ -34,6 +41,13 @@ def gtest(observed, expected):
     .. math::  G = 2\sum_{i=0}^{n-1} O_{i} \log(O_{i}/E_{i})
 
     """
+    if sum_rel_tol is None:
+        sum_rel_tol = 10*mp.eps
+    observed = [mp.mpf(t) for t in observed]
+    expected = [mp.mpf(t) for t in expected]
+    if not mp.almosteq(sum(observed), sum(expected),
+                       rel_eps=sum_rel_tol, abs_eps=0):
+        raise ValueError('sum(observed) differs from sum(expected)')
     stat = sum(2*xlogy(obs, obs/exp) for obs, exp in zip(observed, expected))
     df = len(observed) - 1
     p = mp.gammainc(df/2, stat/2) / mp.gamma(df/2)
