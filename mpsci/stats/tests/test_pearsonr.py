099ca38b2b335ab2ab19175085d1c008c895264e
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-08 13:50:50 -0400

    TST: Test a pearsonr edge case (sample length 2)

--- mpsci/stats/tests/test_pearsonr.py
+++ mpsci/stats/tests/test_pearsonr.py
@@ -12,3 +12,11 @@ def test_pearsonr():
     # "more digits" option that other Alpha computations provide.
     assert mp.almosteq(r, 0.970569, rel_eps=5e-6, abs_eps=0)
     assert mp.almosteq(p, 0.029431, rel_eps=5e-5, abs_eps=0)
+
+
+def test_sample_length_2():
+    x = [1.0, 2.0]
+    y = [0.0, -1.0]
+    r, p = pearsonr(x, y)
+    assert r == -1
+    assert p == 1
