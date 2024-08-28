44ad33db7ac6bdc7d925f31e7fd22f30a5a12928
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-28 15:05:19 -0400

    ENH: Implement mle for the burr12 distribution.

--- mpsci/distributions/burr12.py
+++ mpsci/distributions/burr12.py
@@ -5,7 +5,9 @@ Burr type XII probability distribution
 """
 
 from mpmath import mp
-from mpsci.distributions._common import _validate_p,  _validate_x_bounds
+from mpsci.stats import mean as _fmean
+from mpsci.distributions._common import (_validate_p,  _validate_x_bounds,
+                                         Initial, isfixed)
 
 
 __all__ = ['pdf', 'logpdf',
@@ -183,3 +185,203 @@ def nll(x, c, d, scale):
         c, d, scale = _validate_params(c, d, scale)
         x = _validate_x_bounds(x, low=0, strict_low=True, high=mp.inf)
         return -mp.fsum([logpdf(xi, c, d, scale) for xi in x])
+
+
+# Work in progress...
+
+
+def _nll_grad(x, c, d, scale):
+    n = len(x)
+    sumlogx = mp.fsum([mp.log(xi) for xi in x])
+    z = [(xi/scale) for xi in x]
+    r = [zi**c/(1 + zi**c) for zi in z]
+    return [n/c - n*mp.log(scale) + sumlogx
+            - (d + 1)*mp.fsum([ri*mp.log(zi) for ri, zi in zip(r, z)]),
+            n/d - mp.fsum([mp.log1p(zi**c) for zi in z]),
+            (-c*n + c*(d + 1)*mp.fsum(r))/scale]
+
+
+def _mle_c_d_scale(x, c0=1, d0=1, scale0=1):
+
+    def _first_order_eqs(c, d, scale):
+        return _nll_grad(x, c, d, scale)
+
+    c, d, scale = mp.findroot(_first_order_eqs, [c0, d0, scale0])
+    return c, d, scale
+
+
+def _mle_d_scale(x, c, d0=1, scale0=1):
+
+    def _first_order_eqs(d, scale):
+        return _nll_grad(x, c, d, scale)[1:]
+
+    d, scale = mp.findroot(_first_order_eqs, [d0, scale0])
+    return d, scale
+
+
+def _mle_c_scale(x, d, c0=1, scale0=1):
+
+    def _first_order_eqs(c, scale):
+        v = _nll_grad(x, c, d, scale)
+        return [v[0], v[2]]
+
+    c, scale = mp.findroot(_first_order_eqs, [c0, scale0])
+    return c, scale
+
+
+@mp.extradps(5)
+def _scale_eq_c_and_d_fixed(scale, x, c, d):
+    scale = mp.mpf(scale)
+    x = [mp.mpf(t) for t in x]
+    c = mp.mpf(c)
+    d = mp.mpf(d)
+    q = [(t/scale)**c for t in x]
+    w = [t/(1 + t) for t in q]
+    m = _fmean(w)
+    return 1 - (d + 1)*m
+
+
+@mp.extradps(5)
+def _find_scale_bracket_c_and_d_fixed(x, c, d):
+    scale0 = mp.zero
+    scale1 = mp.eps
+    v = _scale_eq_c_and_d_fixed(scale1, x, c, d)
+    while v < 0:
+        scale0 = scale1
+        scale1 *= 4
+        v = _scale_eq_c_and_d_fixed(scale1, x, c, d)
+    return scale0, scale1
+
+
+@mp.extradps(5)
+def _c_eq_d_and_scale_fixed(c, x, d, scale):
+    x = [mp.mpf(t) for t in x]
+    c = mp.mpf(c)
+    d = mp.mpf(d)
+    scale = mp.mpf(scale)
+    n = len(x)
+    z = [xi/scale for xi in x]
+    logz = [mp.log(zi) for zi in z]
+    return (n/c + mp.fsum(logz)
+            - (d + 1)*mp.fsum([mp.sigmoid(c*mp.log(zi))*mp.log(zi) for zi in z]))
+
+
+@mp.extradps(5)
+def _find_c_bracket_d_and_scale_fixed(x, d, scale):
+    c0 = mp.zero
+    c1 = mp.eps
+    v = _c_eq_d_and_scale_fixed(c1, x, d, scale)
+    while v > 0:
+        c0 = c1
+        c1 *= 4
+        v = _c_eq_d_and_scale_fixed(c1, x, d, scale)
+    return c0, c1
+
+
+@mp.extradps(5)
+def _c_eq_scale_fixed(c, x, scale):
+    x = [mp.mpf(t) for t in x]
+    c = mp.mpf(c)
+    scale = mp.mpf(scale)
+    n = len(x)
+    z = [xi/scale for xi in x]
+    logz = [mp.log(zi) for zi in z]
+    d = 1/_fmean([mp.log1p(zi**c) for zi in z])
+    return (n/c + mp.fsum(logz)
+            - (d + 1)*mp.fsum([mp.sigmoid(c*mp.log(zi))*mp.log(zi) for zi in z]))
+
+
+@mp.extradps(5)
+def _find_c_bracket_scale_fixed(x, scale):
+    c0 = mp.zero
+    c1 = mp.eps
+    v = _c_eq_scale_fixed(c1, x, scale)
+    while v > 0:
+        c0 = c1
+        c1 *= 4
+        v = _c_eq_scale_fixed(c1, x, scale)
+    return c0, c1
+
+
+@mp.extradps(5)
+def mle(x, c=None, d=None, scale=None):
+    """
+    Maximum likelihood estimate for the Burr type XII distribution.
+
+    `x` must be a sequence of numbers.
+    """
+    # The location parameter isn't implemented, so the support
+    # is (0, inf).
+    x = _validate_x_bounds(x, low=0, strict_low=True, high=mp.inf)
+    c_fixed = isfixed(c)
+    d_fixed = isfixed(d)
+    scale_fixed = isfixed(scale)
+
+    if c_fixed and d_fixed and scale_fixed:
+        # All parameters fixed, nothing to do.
+        c, d, scale = _validate_params(c, d, scale)
+        return c, d, scale
+
+    elif c_fixed and not d_fixed and not scale_fixed:
+        # Not working???
+        d0 = d.initial if isinstance(d, Initial) else 1
+        scale0 = scale.initial if isinstance(scale, Initial) else 1
+        c, d0, scale0 = _validate_params(c, d0, scale0)
+        d, scale = _mle_d_scale(x, c, d0, scale0)
+        return c, d, scale
+
+    elif c_fixed and d_fixed and not scale_fixed:
+        # Fit the scale; c and d are fixed.
+        scale0 = scale.initial if isinstance(scale, Initial) else 1
+        c, d, scale0 = _validate_params(c, d, scale0)
+        scale_low, scale_high = _find_scale_bracket_c_and_d_fixed(x, c, d)
+        scale = mp.findroot(lambda s: _scale_eq_c_and_d_fixed(s, x, c, d),
+                            (scale_low, scale_high), method='anderson')
+        return c, d, scale
+
+    elif c_fixed and not d_fixed and scale_fixed:
+        d0 = d.initial if isinstance(d, Initial) else 1
+        # d0 is validated, but it is ignored, because we have an explicit
+        # formula in this case and there is no need for an iterative solver.
+        c, d0, scale = _validate_params(c, d0, scale)
+        d = 1/_fmean([mp.log1p((xi/scale)**c) for xi in x])
+        return c, d, scale
+
+    elif not c_fixed and d_fixed and scale_fixed:
+        c0 = c.initial if isinstance(c, Initial) else 1
+        c0, d, scale = _validate_params(c0, d, scale)
+        c_low, c_high = _find_c_bracket_d_and_scale_fixed(x, d, scale)
+        c = mp.findroot(lambda t: _c_eq_d_and_scale_fixed(t, x, d, scale),
+                        (c_low, c_high), method='anderson')
+        return c, d, scale
+
+    elif not c_fixed and not d_fixed and scale_fixed:
+        # Only the scale is fixed.
+        c0 = c.initial if isinstance(c, Initial) else 1
+        d0 = d.initial if isinstance(d, Initial) else 1
+        c0, d0, scale = _validate_params(c0, d0, scale)
+        c_low, c_high = _find_c_bracket_scale_fixed(x, scale)
+        c = mp.findroot(lambda t: _c_eq_scale_fixed(t, x, scale),
+                        (c_low, c_high), method='anderson')
+        d = 1/_fmean([mp.log1p((xi/scale)**c) for xi in x])
+        return c, d, scale
+
+    elif not c_fixed and d_fixed and not scale_fixed:
+        # work in progress...
+        c0 = c.initial if isinstance(c, Initial) else 1
+        scale0 = scale.initial if isinstance(scale, Initial) else 1
+        c0, d, scale0 = _validate_params(c0, d, scale0)
+        c, scale = _mle_c_scale(x, d, c0, scale0)
+        return c, d, scale
+
+    elif not c_fixed and not d_fixed and not scale_fixed:
+        # Fit c, d and scale
+        c0 = c.initial if isinstance(c, Initial) else 1
+        d0 = d.initial if isinstance(d, Initial) else 1
+        scale0 = scale.initial if isinstance(scale, Initial) else 1
+        c0, d0, scale0 = _validate_params(c0, d0, scale0)
+        c, d, scale = _mle_c_d_scale(x, c0, d0, scale0)
+        return c, d, scale
+
+    else:
+        raise NotImplementedError
--- mpsci/distributions/tests/test_burr12.py
+++ mpsci/distributions/tests/test_burr12.py
@@ -1,7 +1,8 @@
 
 import pytest
 from mpmath import mp
-from mpsci.distributions import burr12
+from mpsci.distributions import burr12, Initial
+from ._utils import check_mle
 
 
 @mp.workdps(25)
@@ -155,3 +156,80 @@ def test_mode0_basic():
     scale = mp.mpf(7)
     mode = burr12.mode(c, d, scale)
     assert mode == 0
+
+
+@pytest.mark.parametrize(
+    'x, c0, d0, scale0',
+    [([1, 2, 3, 4, 5, 8, 13], 1.6, 3.7, 11.2),
+     ([0.090, 0.053, 0.202, 0.165, 0.853, 0.310, 1.181, 0.136, 0.248,
+       0.030, 0.128, 0.130, 0.175, 0.270, 0.377, 0.178, 0.815, 0.223,
+       0.147, 0.627, 1.514, 0.397, 0.014, 0.199, 0.206, 0.391, 1.160,
+       0.210, 0.184, 0.311, 0.210, 0.062, 0.345, 0.847, 0.398, 0.159,
+       0.220, 0.173, 0.061, 0.324, 0.145, 0.190, 0.143, 0.411, 0.317,
+       0.130, 0.296, 0.008, 0.320, 0.271, 0.134, 0.077, 0.189, 0.263,
+       0.554, 0.780, 0.066, 0.670, 1.235, 0.240],
+      1.5, 1.5, 0.25)]
+)
+@mp.workdps(50)
+def test_mle(x, c0, d0, scale0):
+    p_hat = burr12.mle(x, c=Initial(c0), d=Initial(d0), scale=Initial(scale0))
+    check_mle(burr12.nll, x, p_hat)
+
+
+def test_mle_c_fixed():
+    x = [1, 1, 2, 3, 5, 8, 13, 21]
+    c = 1.5
+    d0 = 1.125
+    scale0 = 4.5
+    p_hat = burr12.mle(x, c=c, d=Initial(d0), scale=Initial(scale0))
+    check_mle(lambda x, d, scale: burr12.nll(x, c, d, scale), x, p_hat[1:])
+
+
+@pytest.mark.parametrize(
+    'x, c, d',
+    [([1, 1, 2, 3, 5, 8, 13, 21], 1.5, 1.125),
+     ([1.48016889e-96, 3.54563945e-77, 4.68530581e-99, 3.00079303e-73,
+       8.55871035e-91, 4.65244972e-63, 5.22063674e-59, 3.66513438e-68,
+       4.46739790e-82, 9.63256794e-61, 3.11272713e-94, 4.01509576e-66],
+      0.03125, 200)]
+)
+def test_mle_c_and_d_fixed(x, c, d):
+    p_hat = burr12.mle(x, c=c, d=d)
+    check_mle(lambda x, scale: burr12.nll(x, c, d, scale), x, p_hat[2:])
+
+
+@pytest.mark.parametrize(
+    'x, d, scale',
+    [([0.5, 1.0, 1.5, 3.0, 8.0, 24.0], 0.5, 2.0),
+     ([0.335, 0.595, 0.048, 0.515, 0.043, 0.129, 0.541, 0.276, 0.258,
+       0.056, 0.251, 0.074, 0.011, 0.126, 0.578, 0.263, 0.555, 0.260],
+      20, 5)]
+)
+def test_mle_d_and_scale_fixed(x, d, scale):
+    p_hat = burr12.mle(x, d=d, scale=scale)
+    check_mle(lambda x, c: burr12.nll(x, c, d, scale), x, p_hat[:1])
+
+
+@pytest.mark.parametrize(
+    'x, scale',
+    [([0.243, 1.181, 0.330, 0.172, 0.496, 0.122, 0.380, 0.019, 0.337,
+       0.112, 0.237, 0.537, 0.036, 1.837, 0.148, 0.564, 0.696, 0.435,
+       1.108, 0.134, 0.005, 2.136, 0.075, 0.253], 5),
+     ([2.200, 9.368, 6.731, 4.264, 4.629, 4.355, 5.841, 2.551, 3.809], 0.125)]
+)
+def test_mle_scale_fixed(x, scale):
+    p_hat = burr12.mle(x, scale=scale)
+    check_mle(lambda x, c, d: burr12.nll(x, c, d, scale), x, p_hat[:2])
+
+
+@pytest.mark.parametrize(
+    'x, d, c0, scale0',
+    [([0.116, 0.034, 0.081, 0.051, 0.171, 0.107, 0.171, 0.106, 0.116],
+      1.5, 3.25, 0.12),
+     ([12.5, 18.75, 6.25, 125.0, 37.5],
+      0.125, 6.2, 6.5)]
+)
+def test_mle_d_fixed(x, d, c0, scale0):
+    p_hat = burr12.mle(x, c=Initial(c0), d=d, scale=Initial(scale0))
+    check_mle(lambda x, c, scale: burr12.nll(x, c, d, scale), x,
+              [p_hat[0], p_hat[2]])
