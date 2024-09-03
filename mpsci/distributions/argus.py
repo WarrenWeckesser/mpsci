7ef8941c8502156cad8b48aa0a57199fc57909fe
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-03 12:59:18 -0400

    MAINT: Rename argus 'c' parameter to 'scale'.

--- mpsci/distributions/argus.py
+++ mpsci/distributions/argus.py
@@ -5,7 +5,8 @@ ARGUS distribution
 The parameters, chi and c, follow the conventions used in the
 wikipedia article https://en.wikipedia.org/wiki/ARGUS_distribution.
 
-Note that c is a scale parameter.
+Note that the parameter c in the wikipedia article is called ``scale``
+here.
 """
 
 from mpmath import mp
@@ -14,12 +15,12 @@ from mpmath import mp
 __all__ = ['pdf', 'logpdf', 'cdf', 'sf', 'support', 'mean', 'var', 'mode']
 
 
-def _validate_params(chi, c):
+def _validate_params(chi, scale):
     if chi < 0:
         raise ValueError('chi must be nonnegative')
-    if c <= 0:
-        raise ValueError('c must be positive')
-    return mp.mpf(chi), mp.mpf(c)
+    if scale <= 0:
+        raise ValueError('scale must be positive')
+    return mp.mpf(chi), mp.mpf(scale)
 
 
 def _powm1(x, b):
@@ -30,51 +31,51 @@ def _psi(chi):
     return mp.ncdf(chi) - chi*mp.npdf(chi) - mp.mpf('0.5')
 
 
-def pdf(x, chi, c):
+def pdf(x, chi, scale):
     """
     PDF of the ARGUS probability distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         x = mp.mpf(x)
-        if x < 0 or x > c:
+        if x < 0 or x > scale:
             return mp.zero
-        return mp.exp(logpdf(x, chi, c))
+        return mp.exp(logpdf(x, chi, scale))
 
 
-def logpdf(x, chi, c):
+def logpdf(x, chi, scale):
     """
     Logarithm of the PDF of the ARGUS probability distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         x = mp.mpf(x)
-        if x < 0 or x > c:
+        if x < 0 or x > scale:
             return mp.ninf
-        z = x/c
+        z = x/scale
         if chi == 0:
             return (mp.log(3) + mp.log(z) + 0.5*(mp.log1p(-z) + mp.log1p(z))
-                    - mp.log(c))
+                    - mp.log(scale))
         else:
             t1 = 3*mp.log(chi) - mp.log(2*mp.pi)/2 - mp.log(_psi(chi))
-            t2 = -mp.log(c) + mp.log(z)
+            t2 = -mp.log(scale) + mp.log(z)
             t3 = mp.log1p(-z**2)/2
             t4 = -chi**2/2*(1 - z)*(1 + z)
             return t1 + t2 + t3 + t4
 
 
-def cdf(x, chi, c):
+def cdf(x, chi, scale):
     """
     CDF of the ARGUS probability distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         x = mp.mpf(x)
         if x < 0:
             return mp.zero
-        if x > c:
+        if x > scale:
             return mp.one
-        z = x/c
+        z = x/scale
         if chi == 0:
             # 1 - (1 - z**2)**1.5
             return -_powm1((1 + z)*(1 - z), 1.5)
@@ -82,74 +83,74 @@ def cdf(x, chi, c):
             return mp.one - _psi(chi*mp.sqrt((1 - z)*(1 + z))) / _psi(chi)
 
 
-def sf(x, chi, c):
+def sf(x, chi, scale):
     """
     Survival function of the ARGUS probability distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         x = mp.mpf(x)
         if x < 0:
             return mp.one
-        if x > c:
+        if x > scale:
             return mp.zero
-        z = x/c
+        z = x/scale
         if chi == 0:
             return mp.power((1 + z)*(1 - z), 1.5)
         else:
             return _psi(chi*mp.sqrt((1 - z)*(1 + z))) / _psi(chi)
 
 
-def support(chi, c):
+def support(chi, scale):
     """
     Support of the ARGUS probability distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
-        return (mp.zero, c)
+        chi, scale = _validate_params(chi, scale)
+        return (mp.zero, scale)
 
 
-def mean(chi, c):
+def mean(chi, scale):
     """
     Mean of the ARGUS distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         if chi == 0:
-            return c*3*mp.pi/16
+            return scale*3*mp.pi/16
         else:
             chi2o4 = chi**2/4
-            p1 = c*mp.sqrt(mp.pi/8)
+            p1 = scale*mp.sqrt(mp.pi/8)
             p2 = chi*mp.exp(-chi2o4)
             p3 = mp.besseli(1, chi2o4)
             return p1 * p2 * p3 / _psi(chi)
 
 
-def var(chi, c):
+def var(chi, scale):
     """
     Variance of the ARGUS distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
-        mu = mean(chi, c)
+        chi, scale = _validate_params(chi, scale)
+        mu = mean(chi, scale)
         if chi == 0:
             # TO DO: Derive this value.
-            return c**2*(mp.mpf('2/5') - 9*mp.pi**2/256)
+            return scale**2*(mp.mpf('2/5') - 9*mp.pi**2/256)
         else:
-            t1 = c**2 * (mp.one - 3/chi**2 + chi*mp.npdf(chi)/_psi(chi))
+            t1 = scale**2 * (mp.one - 3/chi**2 + chi*mp.npdf(chi)/_psi(chi))
             return t1 - mu**2
 
 
-def mode(chi, c):
+def mode(chi, scale):
     """
     Mode of the ARGUS distribution.
     """
     with mp.extradps(5):
-        chi, c = _validate_params(chi, c)
+        chi, scale = _validate_params(chi, scale)
         if chi == 0:
-            return c/mp.sqrt(2)
+            return scale/mp.sqrt(2)
         else:
             chi2 = chi**2
-            p1 = c/mp.sqrt(2)/chi
+            p1 = scale/mp.sqrt(2)/chi
             p2 = mp.sqrt(chi2 - 2 + mp.sqrt(chi2**2 + 4))
             return p1 * p2
--- mpsci/distributions/tests/test_argus.py
+++ mpsci/distributions/tests/test_argus.py
@@ -5,10 +5,10 @@ from mpsci.distributions import argus
 from ._expect import noncentral_moment_with_integral
 
 
-@pytest.mark.parametrize('c', [-3.0, 0.0])
-def test_validate_c(c):
-    with pytest.raises(ValueError, match='c must be positive'):
-        argus._validate_params(4.5, c)
+@pytest.mark.parametrize('scale', [-3.0, 0.0])
+def test_validate_scale(scale):
+    with pytest.raises(ValueError, match='scale must be positive'):
+        argus._validate_params(4.5, scale)
 
 
 def test_validate_chi():
@@ -18,7 +18,7 @@ def test_validate_chi():
 
 def test_support():
     s = argus.support(1.0, 0.5)
-    # The support is just (0, c).
+    # The support is just (0, scale).
     assert s == (mp.zero, mp.mpf(0.5))
 
 
@@ -55,47 +55,47 @@ def test_sf_outside_support():
 
 @mp.workdps(50)
 def test_pdf_integral_is_one():
-    c = 0.5
-    i = mp.quad(lambda t: argus.pdf(t, 1.0, c), [0, c])
+    scale = 0.5
+    i = mp.quad(lambda t: argus.pdf(t, 1.0, scale), [0, scale])
     assert mp.almosteq(i, mp.one)
 
 
-@pytest.mark.parametrize('x, chi, c', [(0.25, 3, 0.5), (0.95, 2.5, 1.0)])
+@pytest.mark.parametrize('x, chi, scale', [(0.25, 3, 0.5), (0.95, 2.5, 1.0)])
 @mp.workdps(50)
-def test_pdf_integrates_to_cdf(x, chi, c):
-    p = argus.cdf(x, chi, c)
-    q = mp.quad(lambda t: argus.pdf(t, chi, c), [0, x])
+def test_pdf_integrates_to_cdf(x, chi, scale):
+    p = argus.cdf(x, chi, scale)
+    q = mp.quad(lambda t: argus.pdf(t, chi, scale), [0, x])
     assert mp.almosteq(p, q), f'{p=} is not close to {q=}'
 
 
-@pytest.mark.parametrize('x, chi, c', [(0.25, 3, 0.5), (0.95, 2.5, 1.0)])
+@pytest.mark.parametrize('x, chi, scale', [(0.25, 3, 0.5), (0.95, 2.5, 1.0)])
 @mp.workdps(50)
-def test_pdf_integrates_to_sf(x, chi, c):
-    p = argus.sf(x, chi, c)
-    q = mp.quad(lambda t: argus.pdf(t, chi, c), [x, c])
+def test_pdf_integrates_to_sf(x, chi, scale):
+    p = argus.sf(x, chi, scale)
+    q = mp.quad(lambda t: argus.pdf(t, chi, scale), [x, scale])
     assert mp.almosteq(p, q), f'{p=} is not close to {q=}'
 
 
-@pytest.mark.parametrize('chi, c',
+@pytest.mark.parametrize('chi, scale',
                          [(0.5, 3.0),
                           (0.125, 25.0),
                           (10, 4),
                           (240, 725)])
 @mp.workdps(50)
-def test_mean_with_integral(chi, c):
-    m = argus.mean(chi, c)
-    q = noncentral_moment_with_integral(1, argus, (chi, c))
+def test_mean_with_integral(chi, scale):
+    m = argus.mean(chi, scale)
+    q = noncentral_moment_with_integral(1, argus, (chi, scale))
     assert mp.almosteq(m, q)
 
 
 @mp.workdps(50)
 def test_var_with_integral():
     chi = 3.5
-    c = 1.25
-    var = argus.var(chi, c)
+    scale = 1.25
+    var = argus.var(chi, scale)
 
-    mean = argus.mean(chi, c)
-    mom2 = noncentral_moment_with_integral(2, argus, (chi, c))
+    mean = argus.mean(chi, scale)
+    mom2 = noncentral_moment_with_integral(2, argus, (chi, scale))
     assert mp.almosteq(var, mom2 - mean**2)
 
 
@@ -103,9 +103,9 @@ def test_var_with_integral():
 def test_mode():
     # A crude test of the mode.
     chi = 4.0
-    c = 0.125
-    m = argus.mode(chi, c)
-    pm = argus.pdf(m, chi, c)
+    scale = 0.125
+    m = argus.mode(chi, scale)
+    pm = argus.pdf(m, chi, scale)
     delta = mp.sqrt(mp.eps)
-    assert argus.pdf(m - delta, chi, c) < pm
-    assert argus.pdf(m + delta, chi, c) < pm
+    assert argus.pdf(m - delta, chi, scale) < pm
+    assert argus.pdf(m + delta, chi, scale) < pm
