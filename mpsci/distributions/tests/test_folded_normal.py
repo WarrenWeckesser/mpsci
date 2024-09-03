39e80f6f0cc1c9e4d594c37c856f274858f9b28f
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-03 17:59:30 -0400

    TST: Add more tests for the folded_normal distribution functions.

--- mpsci/distributions/tests/test_folded_normal.py
+++ mpsci/distributions/tests/test_folded_normal.py
@@ -1,6 +1,27 @@
-
+import pytest
 from mpmath import mp
 from mpsci.distributions import folded_normal
+from ._expect import expect
+
+
+def test_bad_sigma():
+    with pytest.raises(ValueError, match='must be positive'):
+        folded_normal.pdf(3, 4, -5)
+
+
+def test_support():
+    mu = 1
+    sigma = 3
+    sup = folded_normal.support(mu, sigma)
+    assert sup == (0, mp.inf)
+
+
+def test_pdf_logpdf_out_of_support():
+    mu = 2
+    sigma = 8
+    x = -1
+    assert folded_normal.pdf(x, mu, sigma) == 0
+    assert folded_normal.logpdf(x, mu, sigma) == mp.ninf
 
 
 @mp.workdps(80)
@@ -43,3 +64,32 @@ def test_sf():
     # expected = mp.one - (mp.ncdf(x, mu, sigma) - mp.ncdf(-x, mu, sigma))
     expected = mp.ncdf(-x, -mu, sigma) + mp.ncdf(-x, mu, sigma)
     assert mp.almosteq(sf, expected)
+
+
+def test_cdf_sf_out_of_support():
+    mu = 2
+    sigma = 8
+    x = -1
+    assert folded_normal.cdf(x, mu, sigma) == 0
+    assert folded_normal.sf(x, mu, sigma) == 1
+
+
+@mp.workdps(80)
+def test_mean_with_integral():
+    mu = 5
+    sigma = 0.5
+    m = folded_normal.mean(mu, sigma)
+    expected = expect(folded_normal, (mu, sigma), lambda t: t,
+                      support=[0, mp.inf])
+    assert mp.almosteq(m, expected)
+
+
+@mp.workdps(80)
+def test_var_with_integral():
+    mu = 5
+    sigma = 0.5
+    mean = folded_normal.mean(mu, sigma)
+    v = folded_normal.var(mu, sigma)
+    expected = expect(folded_normal, (mu, sigma), lambda t: (t - mean)**2,
+                      support=[0, mp.inf])
+    assert mp.almosteq(v, expected)
