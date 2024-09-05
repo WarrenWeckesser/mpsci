b5aaefb2cc0305c2dab119cdc99cf985d5f1e7ec
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-05 19:47:43 -0400

    TST: Add more tests for exponweib functions.

--- mpsci/distributions/tests/test_exponweib.py
+++ mpsci/distributions/tests/test_exponweib.py
@@ -3,6 +3,67 @@ from mpmath import mp
 from mpsci.distributions import exponweib
 
 
+def test_support():
+    a = 1.25
+    c = 2.75
+    scale = 3
+    sup = exponweib.support(a, c, scale=scale)
+    assert sup == (mp.zero, mp.inf)
+
+
+def test_bad_params():
+    with pytest.raises(ValueError, match='`a` must be greater than 0'):
+        exponweib.pdf(1, 0, 2, 3)
+    with pytest.raises(ValueError, match='`c` must be greater than 0'):
+        exponweib.pdf(1, 1, 0, 3)
+    with pytest.raises(ValueError, match='`scale` must be greater than 0'):
+        exponweib.pdf(1, 1, 2, -3)
+
+
+def test_pdf_logpdf_out_of_support():
+    a = 3.25
+    b = 0.25
+    scale = 0.125
+    x = -1.5
+    p = exponweib.pdf(x, a, b, scale)
+    assert p == 0
+    logp = exponweib.logpdf(x, a, b, scale)
+    assert logp == mp.ninf
+
+
+@mp.workdps(60)
+def test_pdf_normalization():
+    integral = mp.quad(lambda t: exponweib.pdf(t, 1.25, 2, scale=0.5),
+                       [0, 0.5, mp.inf],
+                       method='tanh-sinh')
+    assert mp.almosteq(integral, 1)
+
+
+@pytest.mark.parametrize('x', ['1e-250', 8, '1e6'])
+@mp.workdps(60)
+def test_pdf_logpdf_consistency(x):
+    # Consistency check: logpdf(x) ~= log(pdf(x))
+    a = 1.25
+    c = 2.75
+    scale = 3
+    x = mp.mpf(x)
+    logp1 = exponweib.logpdf(x, a, c, scale=scale)
+    with mp.extradps(5):
+        logp2 = mp.log(exponweib.pdf(x, a, c, scale=scale))
+    assert mp.almosteq(logp1, logp2)
+
+
+def test_cf_sf_out_of_support():
+    a = 3.25
+    b = 0.25
+    scale = 0.125
+    x = -1.5
+    p = exponweib.cdf(x, a, b, scale)
+    assert p == 0
+    q = exponweib.sf(x, a, b, scale)
+    assert q == 1
+
+
 @pytest.mark.parametrize(
     'x, a, c, scale',
     [(3, 10, 0.5, 2.5),
