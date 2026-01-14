4abea848f0f2bc89341603e4c17a32d85a57f224
Warren Weckesser <warren.weckesser@gmail.com>
2026-01-14 14:56:01 -0500

    DOC: Use resubber in the faddeevaw docstring.

--- mpsci/fun/_faddeevaw.py
+++ mpsci/fun/_faddeevaw.py
@@ -1,3 +1,4 @@
+import re
 from mpmath import mp
 
 
@@ -11,3 +12,8 @@ def faddeevaw(z):
     """
     z = mp.mpc(z)
     return mp.exp(-z**2) * mp.erfc(-1j*z)
+
+
+faddeevaw._docstring_re_subs = [
+    (r'W\(z\).*z\)', r':math:`W(z) = e^{-z^2}\\textrm{erfc}(-i z)`', 0, 0)
+]
