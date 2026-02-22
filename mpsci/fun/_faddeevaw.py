305fa41c8b7668f71f404a3a25ee5707784355f3
Warren Weckesser <warren.weckesser@gmail.com>
2026-02-22 13:52:18 -0500

    DOC: Add simple examples to the faddeevaw docstring.

--- mpsci/fun/_faddeevaw.py
+++ mpsci/fun/_faddeevaw.py
@@ -9,6 +9,22 @@ def faddeevaw(z):
         W(z) = exp(-z**2) * erfc(-1j * z)
 
     See https://en.wikipedia.org/wiki/Faddeeva_function.
+
+    Examples
+    --------
+    >>> from mpmath import mp
+    >>> from mpsci.fun import faddeevaw
+    >>> mp.dps = 25
+    
+    >>> z = 3 + 1.5j
+    >>> faddeevaw(z)
+    mpc(real='0.0832095352862092579274132846', imag='0.1508797901286885260327614389')
+
+    Verify the identity W(-z) = W(z.conjugate()).conjugate().
+
+    >>> faddeevaw(-z), faddeevaw(z.conjugate()).conjugate()
+    (mpc(real='-0.08534318299726304595760856355', imag='-0.151844872400130328305182211'),
+     mpc(real='-0.08534318299726304595760856355', imag='-0.151844872400130328305182211'))
     """
     z = mp.mpc(z)
     return mp.exp(-z**2) * mp.erfc(-1j*z)
