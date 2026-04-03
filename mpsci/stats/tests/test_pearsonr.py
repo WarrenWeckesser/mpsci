c7d49777e43f610e5609f121ba6860a4d212f7b4
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-03 17:52:28 -0400

    TST: Add a test for pearsonr_ci.

--- mpsci/stats/tests/test_pearsonr.py
+++ mpsci/stats/tests/test_pearsonr.py
@@ -1,5 +1,5 @@
 from mpmath import mp
-from mpsci.stats import pearsonr
+from mpsci.stats import pearsonr, pearsonr_ci
 
 
 def test_pearsonr():
@@ -20,3 +20,14 @@ def test_sample_length_2():
     r, p = pearsonr(x, y)
     assert r == -1
     assert p == 1
+
+
+def test_pearsonr_ci():
+    x = [0, 1, 3, 6, 10, 15, 21, 28, 36, 45]
+    y = [0.0, 0.5625, 2.0625, 5.25, 11.25, 21.5625, 38.0625, 63.0,  99.0, 149.0625]
+    r, p = pearsonr(x, y)
+    rlo, rhi = pearsonr_ci(r, len(x), alpha=0.05)
+    # Reference values were checked with several independent sources.  They
+    # were not high precision, so we check only about 8 significant digits.
+    assert mp.almosteq(rlo, 0.87782892, rel_eps=5e-8, abs_eps=0)
+    assert mp.almosteq(rhi, 0.99330131, rel_eps=5e-8, abs_eps=0)
