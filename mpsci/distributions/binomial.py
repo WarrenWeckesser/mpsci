622f8cce75e43450f78dbbfda672b4ec1d754cb3
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-10 09:59:45 -0400

    MAINT: Use the mp.extradps decorator binomial.py

--- mpsci/distributions/binomial.py
+++ mpsci/distributions/binomial.py
@@ -57,28 +57,29 @@ def support(n, p):
     return range(n + 1)
 
 
+@mp.extradps(5)
 def pmf(k, n, p):
     """
     Probability mass function of the binomial distribution.
     """
-    with mp.extradps(5):
-        n, p = _validate_np(n, p)
-        return (mp.binomial(n, k) *
-                mp.power(p, k) *
-                mp.power(1 - p, n - k))
+    n, p = _validate_np(n, p)
+    return (mp.binomial(n, k) *
+            mp.power(p, k) *
+            mp.power(1 - p, n - k))
 
 
+@mp.extradps(5)
 def logpmf(k, n, p):
     """
     Natural log of the probability mass function of the binomial distribution.
     """
-    with mp.extradps(5):
-        n, p = _validate_np(n, p)
-        return (logbinomial(n, k)
-                + k*mp.log(p)
-                + mp.fsum([n, -k])*mp.log1p(-p))
+    n, p = _validate_np(n, p)
+    return (logbinomial(n, k)
+            + k*mp.log(p)
+            + mp.fsum([n, -k])*mp.log1p(-p))
 
 
+@mp.extradps(5)
 def cdf(k, n, p, method='incbeta'):
     """
     Cumulative distribution function of the binomial distribution.
@@ -92,23 +93,22 @@ def cdf(k, n, p, method='incbeta'):
     if method not in ['sumpmf', 'incbeta']:
         raise ValueError('method must be "sumpmf" or "incbeta"')
     if method == 'incbeta':
-        with mp.extradps(5):
-            n, p = _validate_np(n, p)
-            if k == n:
-                return mp.one
-            # XXX For large values of k and/or n, betainc fails. The failure
-            # occurs in one of the hypergeometric functions.
-            return mp.betainc(n - k, k + 1, x1=0, x2=1 - p,
-                              regularized=True)
+        n, p = _validate_np(n, p)
+        if k == n:
+            return mp.one
+        # XXX For large values of k and/or n, betainc fails. The failure
+        # occurs in one of the hypergeometric functions.
+        return mp.betainc(n - k, k + 1, x1=0, x2=1 - p,
+                            regularized=True)
     else:
         # method is "sumpmf"
-        with mp.extradps(5):
-            n, p = _validate_np(n, p)
-            c = mp.fsum([mp.exp(logpmf(t, n, p))
-                         for t in range(k + 1)])
-            return c
+        n, p = _validate_np(n, p)
+        c = mp.fsum([mp.exp(logpmf(t, n, p))
+                        for t in range(k + 1)])
+        return c
 
 
+@mp.extradps(5)
 def sf(k, n, p, method='incbeta'):
     """
     Survival function of the binomial distribution.
@@ -122,39 +122,38 @@ def sf(k, n, p, method='incbeta'):
     if method not in ['sumpmf', 'incbeta']:
         raise ValueError('method must be "sumpmf" or "incbeta"')
     if method == 'incbeta':
-        with mp.extradps(5):
-            n, p = _validate_np(n, p)
-            # XXX For large values of k and/or n, betainc fails. The failure
-            # occurs in one of the hypergeometric functions.
-            return mp.betainc(n - k, k + 1, x1=1-p, x2=1,
-                              regularized=True)
+        n, p = _validate_np(n, p)
+        # XXX For large values of k and/or n, betainc fails. The failure
+        # occurs in one of the hypergeometric functions.
+        return mp.betainc(n - k, k + 1, x1=1-p, x2=1,
+                            regularized=True)
     else:
         # method is "sumpmf"
-        with mp.extradps(5):
-            n, p = _validate_np(n, p)
-            c = mp.fsum([mp.exp(logpmf(t, n, p))
-                         for t in range(k + 1, n + 1)])
-            return c
+        n, p = _validate_np(n, p)
+        c = mp.fsum([mp.exp(logpmf(t, n, p))
+                        for t in range(k + 1, n + 1)])
+        return c
 
 
+@mp.extradps(5)
 def mean(n, p):
     """
     Mean of the binomial distribution.
     """
-    with mp.extradps(5):
-        n, p = _validate_np(n, p)
-        return n*p
+    n, p = _validate_np(n, p)
+    return n*p
 
 
+@mp.extradps(5)
 def var(n, p):
     """
     Variance of the binomial distribution.
     """
-    with mp.extradps(5):
-        n, p = _validate_np(n, p)
-        return n * p * (1 - p)
+    n, p = _validate_np(n, p)
+    return n * p * (1 - p)
 
 
+@mp.extradps(5)
 def nll(x, n, p, *, counts=None):
     """
     Negative log-likelihood of the binomial distribution.
@@ -162,15 +161,14 @@ def nll(x, n, p, *, counts=None):
     `x` must be a sequence of nonnegative integers, with
     ``0 <= x[i] <= n``.
     """
-    with mp.extradps(5):
-        n, p = _validate_np(n, p)
-        x = _validate_x_bounds(x, low=0, high=n,
-                               strict_low=False, strict_high=False)
-        if not all([mp.isint(t) for t in x]):
-            raise ValueError('all values in x must be integers')
-        counts = _validate_counts(x, counts, expand_none=True)
-        return -mp.fsum([count*logpmf(t, n, p)
-                         for t, count in zip(x, counts)])
+    n, p = _validate_np(n, p)
+    x = _validate_x_bounds(x, low=0, high=n,
+                            strict_low=False, strict_high=False)
+    if not all([mp.isint(t) for t in x]):
+        raise ValueError('all values in x must be integers')
+    counts = _validate_counts(x, counts, expand_none=True)
+    return -mp.fsum([count*logpmf(t, n, p)
+                        for t, count in zip(x, counts)])
 
 
 def _p_threshold(k):
@@ -179,6 +177,7 @@ def _p_threshold(k):
     return -mp.expm1(-d)
 
 
+@mp.extradps(5)
 def mle(x, *, counts=None, n=None, p=None):
     """
     Maximum likelihood estimation for the binomial distribution.
@@ -205,105 +204,104 @@ def mle(x, *, counts=None, n=None, p=None):
     mpf('0.734375')
 
     """
-    with mp.extradps(5):
-        all_int = all([int(xi) == xi for xi in x])
-        if not all_int:
-            raise ValueError('all values in x must be integers')
-        x = _validate_x_bounds(x, low=0, high=mp.inf,
-                               strict_low=False)
-        xmax = max(x)
-        counts = _validate_counts(x, counts, expand_none=False)
-        n_fixed = isfixed(n)
-        p_fixed = isfixed(p)
-        if n_fixed:
-            if xmax > n:
-                raise ValueError(f'The fixed value of n ({n}) must not be '
-                                 f'less than the maximum value in x ({xmax})')
-            if p_fixed:
-                # Other than validation, there is nothing to do.
-                n, p = _validate_np(n, p)
-                return n, p
-            else:
-                # XXX p=Initial(...) is ignored, because we have an explicit
-                # formula. Should Initial be disallowed for p in this case?
-                n, _ = _validate_np(n, 0.5)
-                m = _mean(x, weights=counts)
-                phat = m/n
-                return int(n), phat
-        # n is not fixed.
-        if not p_fixed:
-            if n is not None and n.initial < xmax:
-                raise ValueError(f"Initial guess for n ({n.initial}) must not "
-                                 f"be less than max(x) ({xmax}))")
+    all_int = all([int(xi) == xi for xi in x])
+    if not all_int:
+        raise ValueError('all values in x must be integers')
+    x = _validate_x_bounds(x, low=0, high=mp.inf,
+                            strict_low=False)
+    xmax = max(x)
+    counts = _validate_counts(x, counts, expand_none=False)
+    n_fixed = isfixed(n)
+    p_fixed = isfixed(p)
+    if n_fixed:
+        if xmax > n:
+            raise ValueError(f'The fixed value of n ({n}) must not be '
+                                f'less than the maximum value in x ({xmax})')
+        if p_fixed:
+            # Other than validation, there is nothing to do.
+            n, p = _validate_np(n, p)
+            return n, p
+        else:
+            # XXX p=Initial(...) is ignored, because we have an explicit
+            # formula. Should Initial be disallowed for p in this case?
+            n, _ = _validate_np(n, 0.5)
             m = _mean(x, weights=counts)
+            phat = m/n
+            return int(n), phat
+    # n is not fixed.
+    if not p_fixed:
+        if n is not None and n.initial < xmax:
+            raise ValueError(f"Initial guess for n ({n.initial}) must not "
+                                f"be less than max(x) ({xmax}))")
+        m = _mean(x, weights=counts)
 
-            def mle_n_eqn(n):
-                p1 = m/n
-                return (-_mean([mp.digamma(n - xi + 1) for xi in x],
-                               weights=counts)
-                        + mp.digamma(n + 1) + mp.log1p(-p1))
+        def mle_n_eqn(n):
+            p1 = m/n
+            return (-_mean([mp.digamma(n - xi + 1) for xi in x],
+                            weights=counts)
+                    + mp.digamma(n + 1) + mp.log1p(-p1))
 
-            if n is None:
-                v = _wvar(x, weights=counts)
-                if (m - v) > 0:
-                    n0 = m**2/(m - v)
-                    if n0 < xmax:
-                        n0 = xmax
-                else:
+        if n is None:
+            v = _wvar(x, weights=counts)
+            if (m - v) > 0:
+                n0 = m**2/(m - v)
+                if n0 < xmax:
                     n0 = xmax
             else:
-                n0 = n.initial
-            # print(f"{n0 = }")
-            nhat = mp.findroot(mle_n_eqn, n0)
-            if nhat < xmax:
-                nhat = xmax
-                return mle(x, counts=counts, n=nhat, p=p)
-            phat = m/nhat
-            # Integer n is required.
-            if int(nhat) == nhat:
-                return int(nhat), phat
-            nhat0 = int(mp.floor(nhat))
-            nhat1 = int(mp.ceil(nhat))
-            phat0 = m/(nhat0)
-            phat1 = m/(nhat1)
-            nll0 = nll(x, n=nhat0, p=phat0, counts=counts)
-            nll1 = nll(x, n=nhat1, p=phat1, counts=counts)
-            if nll0 <= nll1:
-                return nhat0, phat0
-            else:
-                return nhat1, phat1
+                n0 = xmax
         else:
-            # n is free, p is fixed.
-            _, p = _validate_np(1, p)
-            pt = _p_threshold(x)
-            if p >= pt:
-                nhat = xmax
-                return int(xmax), p
-            m = _mean(x, weights=counts)
+            n0 = n.initial
+        # print(f"{n0 = }")
+        nhat = mp.findroot(mle_n_eqn, n0)
+        if nhat < xmax:
+            nhat = xmax
+            return mle(x, counts=counts, n=nhat, p=p)
+        phat = m/nhat
+        # Integer n is required.
+        if int(nhat) == nhat:
+            return int(nhat), phat
+        nhat0 = int(mp.floor(nhat))
+        nhat1 = int(mp.ceil(nhat))
+        phat0 = m/(nhat0)
+        phat1 = m/(nhat1)
+        nll0 = nll(x, n=nhat0, p=phat0, counts=counts)
+        nll1 = nll(x, n=nhat1, p=phat1, counts=counts)
+        if nll0 <= nll1:
+            return nhat0, phat0
+        else:
+            return nhat1, phat1
+    else:
+        # n is free, p is fixed.
+        _, p = _validate_np(1, p)
+        pt = _p_threshold(x)
+        if p >= pt:
+            nhat = xmax
+            return int(xmax), p
+        m = _mean(x, weights=counts)
 
-            def mle_n_eqn(n):
-                return (-_mean([mp.digamma(n - xi + 1) for xi in x],
-                               weights=counts)
-                        + mp.digamma(n + 1) + mp.log1p(-p))
+        def mle_n_eqn(n):
+            return (-_mean([mp.digamma(n - xi + 1) for xi in x],
+                            weights=counts)
+                    + mp.digamma(n + 1) + mp.log1p(-p))
 
-            n0 = xmax + 1 if n is None else n.initial
-            try:
-                nhat = mp.findroot(mle_n_eqn, n0)
-            except ValueError:
-                # Try again with a different initial condition.
-                n0 = m/p
-                nhat = mp.findroot(mle_n_eqn, n0)
-            if nhat < xmax:
-                nhat = xmax
-                return int(nhat), p
-            # Integer n is required.
-            if int(nhat) == nhat:
-                return int(nhat), p
-            nhat0 = int(mp.floor(nhat))
-            nhat1 = nhat0 + 1
-            nll0 = nll(x, n=nhat0, p=p, counts=counts)
-            nll1 = nll(x, n=nhat1, p=p, counts=counts)
-            if nll0 <= nll1:
-                return nhat0, p
-            else:
-                return nhat1, p
+        n0 = xmax + 1 if n is None else n.initial
+        try:
+            nhat = mp.findroot(mle_n_eqn, n0)
+        except ValueError:
+            # Try again with a different initial condition.
+            n0 = m/p
+            nhat = mp.findroot(mle_n_eqn, n0)
+        if nhat < xmax:
+            nhat = xmax
+            return int(nhat), p
+        # Integer n is required.
+        if int(nhat) == nhat:
+            return int(nhat), p
+        nhat0 = int(mp.floor(nhat))
+        nhat1 = nhat0 + 1
+        nll0 = nll(x, n=nhat0, p=p, counts=counts)
+        nll1 = nll(x, n=nhat1, p=p, counts=counts)
+        if nll0 <= nll1:
+            return nhat0, p
+        else:
+            return nhat1, p
