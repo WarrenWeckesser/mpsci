9628a48ebf46add3ec620f151f35bfbe81dc3c6c
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-12 16:06:26 -0400

    MAINT: Use the mp.extradps decorator in laplace.py.

--- mpsci/distributions/laplace.py
+++ mpsci/distributions/laplace.py
@@ -21,56 +21,57 @@ def _validate_params(mu, b):
     return mp.mpf(mu), mp.mpf(b)
 
 
+@mp.extradps(5)
 def pdf(x, mu=0, b=1):
     """
     Laplace distribution probability density function.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        x = mp.mpf(x)
-        return mp.exp(-abs(x - mu)/b)/(2*b)
+    mu, b = _validate_params(mu, b)
+    x = mp.mpf(x)
+    return mp.exp(-abs(x - mu)/b)/(2*b)
 
 
+@mp.extradps(5)
 def logpdf(x, mu=0, b=1):
     """
     Log of the PDF of the Laplace distribution.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        x = mp.mpf(x)
-        return -abs(x - mu)/b - mp.log(2*b)
+    mu, b = _validate_params(mu, b)
+    x = mp.mpf(x)
+    return -abs(x - mu)/b - mp.log(2*b)
 
 
+@mp.extradps(5)
 def cdf(x, mu=0, b=1):
     """
     Laplace distribution cumulative distribution function.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        x = mp.mpf(x)
-        z = (x - mu)/b
-        if x <= mu:
-            c = mp.exp(z)/2
-        else:
-            c = 1 - mp.exp(-z)/2
-        return c
+    mu, b = _validate_params(mu, b)
+    x = mp.mpf(x)
+    z = (x - mu)/b
+    if x <= mu:
+        c = mp.exp(z)/2
+    else:
+        c = 1 - mp.exp(-z)/2
+    return c
 
 
+@mp.extradps(5)
 def sf(x, mu=0, b=1):
     """
     Laplace distribution survival function.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        x = mp.mpf(x)
-        z = (x - mu)/b
-        if x <= mu:
-            c = 1 - mp.exp(z)/2
-        else:
-            c = mp.exp(-z)/2
-        return c
+    mu, b = _validate_params(mu, b)
+    x = mp.mpf(x)
+    z = (x - mu)/b
+    if x <= mu:
+        c = 1 - mp.exp(z)/2
+    else:
+        c = mp.exp(-z)/2
+    return c
 
 
+@mp.extradps(5)
 def interval_prob(x1, x2, mu=0, b=1):
     """
     Compute the probability of x in [x1, x2] for the Laplace distribution.
@@ -84,25 +85,25 @@ def interval_prob(x1, x2, mu=0, b=1):
 
     x1 must be less than or equal to x2.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        x1 = mp.mpf(x1)
-        x2 = mp.mpf(x2)
-        if x1 > x2:
-            raise ValueError('x1 must not be greater than x2')
-        if x1 == x2:
-            return mp.zero
-        z1 = (x1 - mu)/b
-        z2 = (x2 - mu)/b
-        delta = z2 - z1
-        if z2 <= 0:
-            return -mp.exp(z2)*mp.expm1(-delta)/2
-        elif z1 >= 0:
-            return -mp.exp(-z1)*mp.expm1(-delta)/2
-        else:
-            return -(mp.expm1(-z2) + mp.expm1(z1))/2
+    mu, b = _validate_params(mu, b)
+    x1 = mp.mpf(x1)
+    x2 = mp.mpf(x2)
+    if x1 > x2:
+        raise ValueError('x1 must not be greater than x2')
+    if x1 == x2:
+        return mp.zero
+    z1 = (x1 - mu)/b
+    z2 = (x2 - mu)/b
+    delta = z2 - z1
+    if z2 <= 0:
+        return -mp.exp(z2)*mp.expm1(-delta)/2
+    elif z1 >= 0:
+        return -mp.exp(-z1)*mp.expm1(-delta)/2
+    else:
+        return -(mp.expm1(-z2) + mp.expm1(z1))/2
 
 
+@mp.extradps(5)
 def invcdf(p, mu=0, b=1):
     """
     Laplace distribution inverse CDF.
@@ -110,57 +111,57 @@ def invcdf(p, mu=0, b=1):
     This function is also known as the quantile function or the percent
     point function.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        p = mp.mpf(p)
-        if p <= 0.5:
-            q = mu + b*mp.log(2*p)
-        else:
-            q = mu - b*mp.log(2 - 2*p)
-        return q
+    mu, b = _validate_params(mu, b)
+    p = mp.mpf(p)
+    if p <= 0.5:
+        q = mu + b*mp.log(2*p)
+    else:
+        q = mu - b*mp.log(2 - 2*p)
+    return q
 
 
+@mp.extradps(5)
 def invsf(p, mu=0, b=1):
     """
     Laplace distribution inverse survival function.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        p = mp.mpf(p)
-        if p >= 0.5:
-            q = mu + b*mp.log(2 - 2*p)
-        else:
-            q = mu - b*mp.log(2*p)
-        return q
+    mu, b = _validate_params(mu, b)
+    p = mp.mpf(p)
+    if p >= 0.5:
+        q = mu + b*mp.log(2 - 2*p)
+    else:
+        q = mu - b*mp.log(2*p)
+    return q
 
 
+@mp.extradps(5)
 def support(mu=0, b=1):
     """
     Support of the Laplace distribution.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        return (mp.ninf, mp.inf)
+    mu, b = _validate_params(mu, b)
+    return (mp.ninf, mp.inf)
 
 
+@mp.extradps(5)
 def mean(mu=0, b=1):
     """
     Mean of the Laplace distribution.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        return mu
+    mu, b = _validate_params(mu, b)
+    return mu
 
 
+@mp.extradps(5)
 def var(mu=0, b=1):
     """
     Variance of the Laplace distribution.
     """
-    with mp.extradps(5):
-        mu, b = _validate_params(mu, b)
-        return 2*b**2
+    mu, b = _validate_params(mu, b)
+    return 2*b**2
 
 
+@mp.extradps(5)
 def mle(x, *, mu=None, b=None):
     """
     Laplace distribution maximum likelihood parameter estimation.
@@ -176,46 +177,46 @@ def mle(x, *, mu=None, b=None):
     estimate for mu.
 
     """
-    with mp.extradps(5):
-        # The estimate of mu is independent of b, so it doesn't
-        # matter if b is fixed or not.
-        if mu is not None:
-            # mu is fixed.
-            mu_est = mp.mpf(mu)
-            x = _seq_to_mp(x)
+    # The estimate of mu is independent of b, so it doesn't
+    # matter if b is fixed or not.
+    if mu is not None:
+        # mu is fixed.
+        mu_est = mp.mpf(mu)
+        x = _seq_to_mp(x)
+    else:
+        # The MLE for mu (the location) is the median of x.
+        # When len(x) is even, it is more correct to say that
+        # the median is *an* estimate rather than *the* estimate,
+        # since the likelihood function does not have a unique
+        # maximum in this case.
+        x = sorted(_seq_to_mp(x))
+        n = len(x)
+        m, r = divmod(n, 2)
+        if r == 1:
+            # n is odd.
+            mu_est = x[m]
         else:
-            # The MLE for mu (the location) is the median of x.
-            # When len(x) is even, it is more correct to say that
-            # the median is *an* estimate rather than *the* estimate,
-            # since the likelihood function does not have a unique
-            # maximum in this case.
-            x = sorted(_seq_to_mp(x))
-            n = len(x)
-            m, r = divmod(n, 2)
-            if r == 1:
-                # n is odd.
-                mu_est = x[m]
-            else:
-                # n is even.
-                # The MLE for mu is not unique.  By convention, we use
-                # the midpoint between x[m-1] and x[m].
-                mu_est = (x[m-1] + x[m])/2
+            # n is even.
+            # The MLE for mu is not unique.  By convention, we use
+            # the midpoint between x[m-1] and x[m].
+            mu_est = (x[m-1] + x[m])/2
 
-        # At this point, we have mu_est, and x has been converted to a list
-        # of mpmath.mpf objects (although we won't need x if b is fixed).
+    # At this point, we have mu_est, and x has been converted to a list
+    # of mpmath.mpf objects (although we won't need x if b is fixed).
 
-        if b is not None:
-            # b is fixed.
-            if b <= 0:
-                raise ValueError('b must be positive.')
-            b_est = mp.mpf(b)
-        else:
-            # b is the mean absolute deviation from mu_est.
-            b_est = _mean([abs(t - mu_est) for t in x])
+    if b is not None:
+        # b is fixed.
+        if b <= 0:
+            raise ValueError('b must be positive.')
+        b_est = mp.mpf(b)
+    else:
+        # b is the mean absolute deviation from mu_est.
+        b_est = _mean([abs(t - mu_est) for t in x])
 
-        return mu_est, b_est
+    return mu_est, b_est
 
 
+@mp.extradps(5)
 def mom(x):
     """
     Method of moments estimation for the Laplace distribution.
@@ -224,7 +225,6 @@ def mom(x):
 
     Returns (mu, b).
     """
-    with mp.extradps(5):
-        M1 = _mean(x)
-        M2 = _mean([mp.mpf(t)**2 for t in x])
-        return M1, mp.sqrt((M2 - M1**2)/2)
+    M1 = _mean(x)
+    M2 = _mean([mp.mpf(t)**2 for t in x])
+    return M1, mp.sqrt((M2 - M1**2)/2)
