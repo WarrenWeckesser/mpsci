3a83f95bbea179b2221a0d86941bb943fdc7d26e
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-12 16:09:50 -0400

    MAINT: Use the mp.extradps decorator in ncx2.py.

--- mpsci/distributions/ncx2.py
+++ mpsci/distributions/ncx2.py
@@ -10,59 +10,60 @@ from ..fun import marcumq, cmarcumq
 __all__ = ['pdf', 'cdf', 'sf', 'support', 'mean', 'var']
 
 
+@mp.extradps(5)
 def pdf(x, k, lam):
     """
     PDF for the noncentral chi-square distribution.
     """
     if x < 0:
         return mp.zero
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k = mp.mpf(k)
-        lam = mp.mpf(lam)
-        p = (mp.exp(-(x + lam)/2) * mp.power(x / lam, (k/2 - 1)/2) *
-             mp.besseli(k/2 - 1, mp.sqrt(lam*x))/2)
+    x = mp.mpf(x)
+    k = mp.mpf(k)
+    lam = mp.mpf(lam)
+    p = (mp.exp(-(x + lam)/2) * mp.power(x / lam, (k/2 - 1)/2) *
+         mp.besseli(k/2 - 1, mp.sqrt(lam*x))/2)
     return p
 
 
+@mp.extradps(5)
 def cdf(x, k, lam):
     """
     CDF for the noncentral chi-square distribution.
     """
     if x <= 0:
         return mp.zero
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k = mp.mpf(k)
-        lam = mp.mpf(lam)
-        c = cmarcumq(k/2, mp.sqrt(lam), mp.sqrt(x))
+    x = mp.mpf(x)
+    k = mp.mpf(k)
+    lam = mp.mpf(lam)
+    c = cmarcumq(k/2, mp.sqrt(lam), mp.sqrt(x))
     return c
 
 
+@mp.extradps(5)
 def sf(x, k, lam):
     """
     Survival function for the noncentral chi-square distribution.
     """
     if x <= 0:
         return mp.one
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k = mp.mpf(k)
-        lam = mp.mpf(lam)
-        s = marcumq(k/2, mp.sqrt(lam), mp.sqrt(x))
+    x = mp.mpf(x)
+    k = mp.mpf(k)
+    lam = mp.mpf(lam)
+    s = marcumq(k/2, mp.sqrt(lam), mp.sqrt(x))
     return s
 
 
+@mp.extradps(5)
 def support(k, lam):
     """
     Support of the noncentral chi-square distribution.
     """
-    with mp.extradps(5):
-        k = mp.mpf(k)
-        lam = mp.mpf(lam)
-        return (mp.zero, mp.inf)
+    k = mp.mpf(k)
+    lam = mp.mpf(lam)
+    return (mp.zero, mp.inf)
 
 
+@mp.extradps(5)
 def mean(k, lam):
     """
     Mean of the noncentral chi-square distribution.
@@ -72,10 +73,11 @@ def mean(k, lam):
     return k + lam
 
 
+@mp.extradps(5)
 def var(k, lam):
     """
     Variance of the noncentral chi-square distribution.
     """
     k = mp.mpf(k)
     lam = mp.mpf(lam)
-    return 2*(k + 2*lam)
+    return 2 * (k + 2 * lam)
