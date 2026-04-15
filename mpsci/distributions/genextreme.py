0552f54bc32d87d861212b2a3e2b60f3ceefedd7
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-15 17:17:55 -0400

    MAINT: Use the mp.extradps decorator in genextreme.py.

--- mpsci/distributions/genextreme.py
+++ mpsci/distributions/genextreme.py
@@ -29,109 +29,110 @@ def _validate_params(xi, mu, sigma):
     return mp.mpf(xi), mp.mpf(mu), mp.mpf(sigma)
 
 
+@mp.extradps(5)
 def pdf(x, xi, mu=0, sigma=1):
     """
     Probability density function of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
 
-        if xi != 0:
-            bound = mu - sigma/xi
-            if xi > 0 and x <= bound:
-                return mp.zero
-            if xi < 0 and x >= bound:
-                return mp.zero
+    if xi != 0:
+        bound = mu - sigma/xi
+        if xi > 0 and x <= bound:
+            return mp.zero
+        if xi < 0 and x >= bound:
+            return mp.zero
 
-        # Formula from wikipedia, which has a sign convention for xi that
-        # is the opposite of scipy's shape parameter.
-        z = (x - mu)/sigma
-        if xi != 0:
-            t = mp.power(1 + z*xi, -1/xi)
-        else:
-            t = mp.exp(-z)
-        p = mp.power(t, xi + 1) * mp.exp(-t) / sigma
-        return p
+    # Formula from wikipedia, which has a sign convention for xi that
+    # is the opposite of scipy's shape parameter.
+    z = (x - mu)/sigma
+    if xi != 0:
+        t = mp.power(1 + z*xi, -1/xi)
+    else:
+        t = mp.exp(-z)
+    p = mp.power(t, xi + 1) * mp.exp(-t) / sigma
+    return p
 
 
+@mp.extradps(5)
 def logpdf(x, xi, mu=0, sigma=1):
     """
     Natural logarithm of the PDF of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
 
-        if xi != 0:
-            bound = mu - sigma/xi
-            if xi > 0 and x <= bound:
-                return mp.ninf
-            if xi < 0 and x >= bound:
-                return mp.ninf
+    if xi != 0:
+        bound = mu - sigma/xi
+        if xi > 0 and x <= bound:
+            return mp.ninf
+        if xi < 0 and x >= bound:
+            return mp.ninf
 
-        # Formula from wikipedia, which has a sign convention for xi that
-        # is the opposite of scipy's shape parameter.
-        z = (x - mu)/sigma
-        if xi != 0:
-            t = mp.power(1 + z*xi, -1/xi)
-            logt = -mp.log1p(z*xi)/xi
-        else:
-            t = mp.exp(-z)
-            logt = -z
-        p = (xi + 1)*logt - t - mp.log(sigma)
-        return p
+    # Formula from wikipedia, which has a sign convention for xi that
+    # is the opposite of scipy's shape parameter.
+    z = (x - mu)/sigma
+    if xi != 0:
+        t = mp.power(1 + z*xi, -1/xi)
+        logt = -mp.log1p(z*xi)/xi
+    else:
+        t = mp.exp(-z)
+        logt = -z
+    p = (xi + 1)*logt - t - mp.log(sigma)
+    return p
 
 
+@mp.extradps(5)
 def cdf(x, xi, mu=0, sigma=1):
     """
     Cumulative distribution function of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        return mp.exp(logcdf(x, xi, mu, sigma))
+    return mp.exp(logcdf(x, xi, mu, sigma))
 
 
+@mp.extradps(5)
 def logcdf(x, xi, mu=0, sigma=1):
     """
     Natural logarithm of the CDF of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
 
-        if xi != 0:
-            bound = mu - sigma/xi
-            if xi > 0 and x <= bound:
-                return mp.ninf
-            if xi < 0 and x >= bound:
-                return mp.zero
+    if xi != 0:
+        bound = mu - sigma/xi
+        if xi > 0 and x <= bound:
+            return mp.ninf
+        if xi < 0 and x >= bound:
+            return mp.zero
 
-        if xi != 0:
-            t = mp.power(1 + ((x - mu)/sigma)*xi, -1/xi)
-        else:
-            t = mp.exp(-(x - mu)/sigma)
-        return -t
+    if xi != 0:
+        t = mp.power(1 + ((x - mu)/sigma)*xi, -1/xi)
+    else:
+        t = mp.exp(-(x - mu)/sigma)
+    return -t
 
 
+@mp.extradps(5)
 def invcdf(p, xi, mu=0, sigma=1):
     """
     Inverse of the CDF of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        p = _validate_p(p)
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi != 0:
-            x = mu + (sigma/xi)*mp.powm1(-mp.log(p), -xi)
-        else:
-            x = mu - sigma*mp.log(-mp.log(p))
-        return x
+    p = _validate_p(p)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi != 0:
+        x = mu + (sigma/xi)*mp.powm1(-mp.log(p), -xi)
+    else:
+        x = mu - sigma*mp.log(-mp.log(p))
+    return x
 
 
+@mp.extradps(5)
 def sf(x, xi, mu=0, sigma=1):
     """
     Survival function of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        return -mp.expm1(logcdf(x, xi, mu, sigma))
+    return -mp.expm1(logcdf(x, xi, mu, sigma))
 
 
+@mp.extradps(5)
 def logsf(x, xi, mu=0, sigma=1):
     """
     Natural log of the survival function of the gen. extreme value distribution.
@@ -145,20 +146,21 @@ def logsf(x, xi, mu=0, sigma=1):
         return mp.log1p(-cdf(x, xi, mu, sigma)).real
 
 
+@mp.extradps(5)
 def invsf(p, xi, mu=0, sigma=1):
     """
     Inverse of the survival function of the generalized extreme value dist.
     """
-    with mp.extradps(5):
-        p = _validate_p(p)
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi != 0:
-            x = mu + (sigma/xi)*mp.powm1(-mp.log1p(-p), -xi)
-        else:
-            x = mu - sigma*mp.log(-mp.log1p(-p))
-        return x
+    p = _validate_p(p)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi != 0:
+        x = mu + (sigma/xi)*mp.powm1(-mp.log1p(-p), -xi)
+    else:
+        x = mu - sigma*mp.log(-mp.log1p(-p))
+    return x
 
 
+@mp.extradps(5)
 def support(xi, mu=0, sigma=1):
     """
     Support of the generalized extreme value distribution.
@@ -170,109 +172,109 @@ def support(xi, mu=0, sigma=1):
     * xi > 0:  [mu - sigma/xi, inf)
 
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi != 0:
-            bound = mu - sigma/xi
-            if xi > 0:
-                return (bound, mp.inf)
-            else:
-                return (mp.ninf, bound)
-        # xi == 0
-        return (mp.ninf, mp.inf)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi != 0:
+        bound = mu - sigma/xi
+        if xi > 0:
+            return (bound, mp.inf)
+        else:
+            return (mp.ninf, bound)
+    # xi == 0
+    return (mp.ninf, mp.inf)
 
 
+@mp.extradps(5)
 def mean(xi, mu=0, sigma=1):
     """
     Mean of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi == 0:
-            return mu + sigma * mp.euler
-        elif xi < 1:
-            g1 = mp.gamma(mp.one - xi)
-            return mu + sigma * (g1 - mp.one)/xi
-        else:
-            return mp.inf
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi == 0:
+        return mu + sigma * mp.euler
+    elif xi < 1:
+        g1 = mp.gamma(mp.one - xi)
+        return mu + sigma * (g1 - mp.one)/xi
+    else:
+        return mp.inf
 
 
+@mp.extradps(5)
 def var(xi, mu=0, sigma=1):
     """
     Variance of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi == 0:
-            return sigma**2 * mp.pi**2 / 6
-        elif 2*xi < 1:
-            g1 = mp.gamma(mp.one - xi)
-            g2 = mp.gamma(mp.one - 2*xi)
-            return sigma**2 * (g2 - g1**2) / xi**2
-        else:
-            return mp.inf
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi == 0:
+        return sigma**2 * mp.pi**2 / 6
+    elif 2*xi < 1:
+        g1 = mp.gamma(mp.one - xi)
+        g2 = mp.gamma(mp.one - 2*xi)
+        return sigma**2 * (g2 - g1**2) / xi**2
+    else:
+        return mp.inf
 
 
+@mp.extradps(5)
 def skewness(xi, mu=0, sigma=1):
     """
     Skewness of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi == 0:
-            return 12*mp.sqrt(6)*mp.zeta(3) / mp.pi**3
-        elif 3*xi < 1:
-            g1 = mp.gamma(mp.one - xi)
-            g2 = mp.gamma(mp.one - 2*xi)
-            g3 = mp.gamma(mp.one - 3*xi)
-            num = g3 - 3*g2*g1 + 2*g1**3
-            den = mp.power(g2 - g1**2, 1.5)
-            return mp.sign(xi)*num/den
-        else:
-            return mp.inf
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi == 0:
+        return 12*mp.sqrt(6)*mp.zeta(3) / mp.pi**3
+    elif 3*xi < 1:
+        g1 = mp.gamma(mp.one - xi)
+        g2 = mp.gamma(mp.one - 2*xi)
+        g3 = mp.gamma(mp.one - 3*xi)
+        num = g3 - 3*g2*g1 + 2*g1**3
+        den = mp.power(g2 - g1**2, 1.5)
+        return mp.sign(xi)*num/den
+    else:
+        return mp.inf
 
 
+@mp.extradps(5)
 def kurtosis(xi, mu=0, sigma=1):
     """
     Excess kurtosis of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if xi == 0:
-            return mp.mpf(12)/5
-        elif 4*xi < 1:
-            g1 = mp.gamma(mp.one - xi)
-            g2 = mp.gamma(mp.one - 2*xi)
-            g3 = mp.gamma(mp.one - 3*xi)
-            g4 = mp.gamma(mp.one - 4*xi)
-            num = g4 - 4*g3*g1 - 3*g2**2 + 12*g2*g1**2 - 6*g1**4
-            den = (g2 - g1**2)**2
-            return num/den
-        else:
-            return mp.inf
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if xi == 0:
+        return mp.mpf(12)/5
+    elif 4*xi < 1:
+        g1 = mp.gamma(mp.one - xi)
+        g2 = mp.gamma(mp.one - 2*xi)
+        g3 = mp.gamma(mp.one - 3*xi)
+        g4 = mp.gamma(mp.one - 4*xi)
+        num = g4 - 4*g3*g1 - 3*g2**2 + 12*g2*g1**2 - 6*g1**4
+        den = (g2 - g1**2)**2
+        return num/den
+    else:
+        return mp.inf
 
 
+@mp.extradps(5)
 def entropy(xi, mu=0, sigma=1):
     """
     Differential entropy of the generalized extreme value distribution.
     """
-    with mp.extradps(5):
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        return mp.log(sigma) + mp.euler*(xi + 1) + 1
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    return mp.log(sigma) + mp.euler*(xi + 1) + 1
 
 
+@mp.extradps(5)
 def _standard_noncentral_moment(n, xi):
-    with mp.extradps(5):
-        if n == 0:
-            return mp.one
-        if xi < mp.one/n:
-            v = [mp.binomial(n, k) * (-1)**k * mp.gamma(1 - xi*k)
-                 for k in range(n+1)]
-            return mp.fsum(v)/(-xi)**n
-        else:
-            return mp.nan
+    if n == 0:
+        return mp.one
+    if xi < mp.one/n:
+        v = [mp.binomial(n, k) * (-1)**k * mp.gamma(1 - xi*k)
+             for k in range(n+1)]
+        return mp.fsum(v)/(-xi)**n
+    else:
+        return mp.nan
 
 
+@mp.extradps(5)
 def noncentral_moment(n, xi, mu=0, sigma=1):
     """
     Noncentral moment of the generalized extreme value distribution.
@@ -282,15 +284,14 @@ def noncentral_moment(n, xi, mu=0, sigma=1):
     # Except for the check xi >= 1/n, this is a generic calculation that
     # could be applied to any loc/scale family if there is a function for
     # the standard (i.e. loc=0, scale=1) noncentral moment.
-    with mp.extradps(5):
-        _validate_moment_n(n)
-        xi, mu, sigma = _validate_params(xi, mu, sigma)
-        if n == 0:
-            return mp.one
-        if xi >= mp.one/n:
-            # Maybe return inf if xi == 1/n?
-            return mp.nan
-        terms = [(mp.binomial(n, k) * mp.power(mu, n - k) * mp.power(sigma, k)
-                  * _standard_noncentral_moment(k, xi))
-                 for k in range(n + 1)]
-        return mp.fsum(terms)
+    _validate_moment_n(n)
+    xi, mu, sigma = _validate_params(xi, mu, sigma)
+    if n == 0:
+        return mp.one
+    if xi >= mp.one/n:
+        # Maybe return inf if xi == 1/n?
+        return mp.nan
+    terms = [(mp.binomial(n, k) * mp.power(mu, n - k) * mp.power(sigma, k)
+              * _standard_noncentral_moment(k, xi))
+             for k in range(n + 1)]
+    return mp.fsum(terms)
