4589efdeccf653d0d4eb2011943f39cf83474984
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-15 14:08:25 -0400

    MAINT: Use the mp.extradps decorator in invgamma.py.

--- mpsci/distributions/invgamma.py
+++ mpsci/distributions/invgamma.py
@@ -34,177 +34,178 @@ def _validate_params(a, loc, scale):
     return a, loc, scale
 
 
+@mp.extradps(5)
 def pdf(x, a, loc=0, scale=1):
     """
     PDF for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        x = mp.mpf(x)
-        if x <= loc:
-            return mp.zero
-        return mp.exp(logpdf(x, a, loc, scale))
+    a, loc, scale = _validate_params(a, loc, scale)
+    x = mp.mpf(x)
+    if x <= loc:
+        return mp.zero
+    return mp.exp(logpdf(x, a, loc, scale))
 
 
+@mp.extradps(5)
 def logpdf(x, a, loc=0, scale=1):
     """
     Logarithm of the PDF for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        x = mp.mpf(x)
-        if x <= loc:
-            return mp.ninf
-        z = (x - loc)/scale
-        logp = -mp.log(scale) - mp.loggamma(a) - (a + 1)*mp.log(z) - 1/z
-        return logp
+    a, loc, scale = _validate_params(a, loc, scale)
+    x = mp.mpf(x)
+    if x <= loc:
+        return mp.ninf
+    z = (x - loc)/scale
+    logp = -mp.log(scale) - mp.loggamma(a) - (a + 1)*mp.log(z) - 1/z
+    return logp
 
 
+@mp.extradps(5)
 def cdf(x, a, loc=0, scale=1):
     """
     CDF for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        x = mp.mpf(x)
-        if x <= loc:
-            return mp.zero
-        z = (x - loc)/scale
-        return mp.gammainc(a, 1/z, mp.inf, regularized=True)
+    a, loc, scale = _validate_params(a, loc, scale)
+    x = mp.mpf(x)
+    if x <= loc:
+        return mp.zero
+    z = (x - loc)/scale
+    return mp.gammainc(a, 1/z, mp.inf, regularized=True)
 
 
+@mp.extradps(5)
 def invcdf(p, a, loc=0, scale=1):
     """
     Inverse of the CDF for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        p = _validate_p(p)
-        if p == 0:
-            return loc
-        if p == 1:
-            return mp.inf
-        # WIP... not updated yet...
-        x0 = mode(a, loc, scale)
-        # FIXME: This loop assumes convergence!
-        while True:
-            x1 = x0 + (p - cdf(x0, a, loc, scale))/pdf(x0, a, loc, scale)
-            if mp.almosteq(x1, x0):
-                break
-            x0 = x1
-        return x1
+    a, loc, scale = _validate_params(a, loc, scale)
+    p = _validate_p(p)
+    if p == 0:
+        return loc
+    if p == 1:
+        return mp.inf
+    # WIP... not updated yet...
+    x0 = mode(a, loc, scale)
+    # FIXME: This loop assumes convergence!
+    while True:
+        x1 = x0 + (p - cdf(x0, a, loc, scale))/pdf(x0, a, loc, scale)
+        if mp.almosteq(x1, x0):
+            break
+        x0 = x1
+    return x1
 
 
+@mp.extradps(5)
 def sf(x, a, loc=0, scale=1):
     """
     Survival function for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        x = mp.mpf(x)
-        if x <= loc:
-            return mp.one
-        z = (x - loc)/scale
-        return mp.gammainc(a, 0, 1/z, regularized=True)
+    a, loc, scale = _validate_params(a, loc, scale)
+    x = mp.mpf(x)
+    if x <= loc:
+        return mp.one
+    z = (x - loc)/scale
+    return mp.gammainc(a, 0, 1/z, regularized=True)
 
 
+@mp.extradps(5)
 def invsf(p, a, loc=0, scale=1):
     """
     Inverse of the survival function for the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        p = _validate_p(p)
-        if p == 0:
-            return mp.inf
-        if p == 1:
-            return loc
-        x0 = mode(a, loc, scale)
-        # FIXME: This loop assumes convergence!
-        while True:
-            x1 = x0 - (p - sf(x0, a, loc, scale))/pdf(x0, a, loc, scale)
-            if mp.almosteq(x1, x0):
-                break
-            x0 = x1
-        return x1
+    a, loc, scale = _validate_params(a, loc, scale)
+    p = _validate_p(p)
+    if p == 0:
+        return mp.inf
+    if p == 1:
+        return loc
+    x0 = mode(a, loc, scale)
+    # FIXME: This loop assumes convergence!
+    while True:
+        x1 = x0 - (p - sf(x0, a, loc, scale))/pdf(x0, a, loc, scale)
+        if mp.almosteq(x1, x0):
+            break
+        x0 = x1
+    return x1
 
 
+@mp.extradps(5)
 def support(a, loc=0, scale=1):
     """
     Support of the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return (loc, mp.inf)
+    a, loc, scale = _validate_params(a, loc, scale)
+    return (loc, mp.inf)
 
 
+@mp.extradps(5)
 def mean(a, loc=0, scale=1):
     """
     Mean of the inverse gamma distribution.
 
     Returns `nan` if `a <= 1`.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return loc + scale / (a - 1) if a > 1 else mp.nan
+    a, loc, scale = _validate_params(a, loc, scale)
+    return loc + scale / (a - 1) if a > 1 else mp.nan
 
 
+@mp.extradps(5)
 def mode(a, loc=0, scale=1):
     """
     Mode of the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return loc + scale / (a + 1)
+    a, loc, scale = _validate_params(a, loc, scale)
+    return loc + scale / (a + 1)
 
 
+@mp.extradps(5)
 def var(a, loc=0, scale=1):
     """
     Variance of the inverse gamma distribution.
 
     Returns `nan` if `a <= 2`.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return scale**2/(a - 1)**2/(a - 2) if a > 2 else mp.nan
+    a, loc, scale = _validate_params(a, loc, scale)
+    return scale**2/(a - 1)**2/(a - 2) if a > 2 else mp.nan
 
 
+@mp.extradps(5)
 def skewness(a, loc=0, scale=1):
     """
     Skewness of the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return 4*mp.sqrt(a - 2)/(a - 3) if a > 3 else mp.nan
+    a, loc, scale = _validate_params(a, loc, scale)
+    return 4*mp.sqrt(a - 2)/(a - 3) if a > 3 else mp.nan
 
 
+@mp.extradps(5)
 def kurtosis(a, loc=0, scale=1):
     """
     Excess kurtosis of the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return 6*(5*a - 11)/(a - 3)/(a - 4) if a > 4 else mp.nan
+    a, loc, scale = _validate_params(a, loc, scale)
+    return 6*(5*a - 11)/(a - 3)/(a - 4) if a > 4 else mp.nan
 
 
+@mp.extradps(5)
 def entropy(a, loc=0, scale=1):
     """
     Differential entropy of the inverse gamma distribution.
     """
-    with mp.extradps(5):
-        a, loc, scale = _validate_params(a, loc, scale)
-        return a + mp.log(scale) + mp.loggamma(a) - (1 + a)*mp.digamma(a)
+    a, loc, scale = _validate_params(a, loc, scale)
+    return a + mp.log(scale) + mp.loggamma(a) - (1 + a)*mp.digamma(a)
 
 
+@mp.extradps(5)
 def _standard_noncentral_moment(n, a):
-    with mp.extradps(5):
-        if n == 0:
-            return mp.one
-        if a <= n:
-            return mp.nan
-        return mp.gammaprod([a - n], [a])
+    if n == 0:
+        return mp.one
+    if a <= n:
+        return mp.nan
+    return mp.gammaprod([a - n], [a])
 
 
+@mp.extradps(5)
 def noncentral_moment(n, a, loc=0, scale=1):
     """
     Noncentral moment of the inverse gamma distribution.
@@ -215,12 +216,11 @@ def noncentral_moment(n, a, loc=0, scale=1):
     # loc/scale family if there is a function for the standard
     # (i.e. loc=0, scale=1) noncentral moment.
     # Cf. genextreme.noncentral_moment()
-    with mp.extradps(5):
-        n = _validate_moment_n(n)
-        a, loc, scale = _validate_params(a, loc, scale)
-        if n == 0:
-            return mp.one
-        terms = [(mp.binomial(n, k) * mp.power(loc, n - k) * mp.power(scale, k)
-                  * _standard_noncentral_moment(k, a))
-                 for k in range(n + 1)]
-        return mp.fsum(terms)
+    n = _validate_moment_n(n)
+    a, loc, scale = _validate_params(a, loc, scale)
+    if n == 0:
+        return mp.one
+    terms = [(mp.binomial(n, k) * mp.power(loc, n - k) * mp.power(scale, k)
+              * _standard_noncentral_moment(k, a))
+             for k in range(n + 1)]
+    return mp.fsum(terms)
