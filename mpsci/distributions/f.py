9e0236ef47d559617fd7cbfac4f4383290f0b75d
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-15 13:09:04 -0400

    MAINT: Use the mp.extradps decorator in f.py.

--- mpsci/distributions/f.py
+++ mpsci/distributions/f.py
@@ -21,118 +21,118 @@ def _validate_params(dfn, dfd):
     return mp.mpf(dfn), mp.mpf(dfd)
 
 
+@mp.extradps(5)
 def pdf(x, dfn, dfd):
     """
     Probability density function of the F distribution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        x = mp.mpf(x)
-        if x <= 0:
-            return mp.zero
-        r = dfn / dfd
-        hdfn = dfn / 2
-        hdfd = dfd / 2
-        p = (r**hdfn
-             * x**(hdfn - 1)
-             * (1 + r*x)**(-(hdfn + hdfd))
-             / mp.beta(hdfn, hdfd))
-        return p
+    dfn, dfd = _validate_params(dfn, dfd)
+    x = mp.mpf(x)
+    if x <= 0:
+        return mp.zero
+    r = dfn / dfd
+    hdfn = dfn / 2
+    hdfd = dfd / 2
+    p = (r**hdfn
+         * x**(hdfn - 1)
+         * (1 + r*x)**(-(hdfn + hdfd))
+         / mp.beta(hdfn, hdfd))
+    return p
 
 
+@mp.extradps(5)
 def logpdf(x, dfn, dfd):
     """
     Natural log. of the probability density function of the F distribution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        x = mp.mpf(x)
-        if x <= 0:
-            return -mp.inf
-        r = dfn / dfd
-        hdfn = dfn / 2
-        hdfd = dfd / 2
-        lp = (hdfn * (mp.log(dfn) - mp.log(dfd))
-              + xlogy(hdfn - 1, x)
-              - xlog1py(hdfn + hdfd, r*x)
-              - logbeta(hdfn, hdfd))
-        return lp
+    dfn, dfd = _validate_params(dfn, dfd)
+    x = mp.mpf(x)
+    if x <= 0:
+        return -mp.inf
+    r = dfn / dfd
+    hdfn = dfn / 2
+    hdfd = dfd / 2
+    lp = (hdfn * (mp.log(dfn) - mp.log(dfd))
+          + xlogy(hdfn - 1, x)
+          - xlog1py(hdfn + hdfd, r*x)
+          - logbeta(hdfn, hdfd))
+    return lp
 
 
+@mp.extradps(5)
 def cdf(x, dfn, dfd):
     """
     Cumulative distribution function of the F distribution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        x = mp.mpf(x)
-        if x <= 0:
-            return mp.zero
-        dfnx = dfn * x
-        return mp.betainc(dfn/2, dfd/2, x2=dfnx/(dfnx + dfd),
-                          regularized=True)
+    dfn, dfd = _validate_params(dfn, dfd)
+    x = mp.mpf(x)
+    if x <= 0:
+        return mp.zero
+    dfnx = dfn * x
+    return mp.betainc(dfn/2, dfd/2, x2=dfnx/(dfnx + dfd),
+                      regularized=True)
 
 
+@mp.extradps(5)
 def invcdf(p, dfn, dfd):
     """
     Inverse of the CDF of the F distriution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        p = _validate_p(p)
-        g = betaincinv(dfn/2, dfd/2, p, method='bisect')
-        # XXX Possible loss of precision in (1 - g):
-        return (dfd/dfn)*g/(1 - g)
+    dfn, dfd = _validate_params(dfn, dfd)
+    p = _validate_p(p)
+    g = betaincinv(dfn/2, dfd/2, p, method='bisect')
+    # XXX Possible loss of precision in (1 - g):
+    return (dfd/dfn)*g/(1 - g)
 
 
+@mp.extradps(5)
 def sf(x, dfn, dfd):
     """
     Survival function of the F distribution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        x = mp.mpf(x)
-        if x <= 0:
-            return mp.one
-        dfnx = dfn * x
-        return mp.betainc(dfn/2, dfd/2, x1=dfnx/(dfnx + dfd),
-                          regularized=True)
+    dfn, dfd = _validate_params(dfn, dfd)
+    x = mp.mpf(x)
+    if x <= 0:
+        return mp.one
+    dfnx = dfn * x
+    return mp.betainc(dfn/2, dfd/2, x1=dfnx/(dfnx + dfd),
+                      regularized=True)
 
 
+@mp.extradps(5)
 def invsf(p, dfn, dfd):
     """
     Inverse of the survival function of the F distriution.
 
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        p = _validate_p(p)
-        g = betaincinv(dfd/2, dfn/2, p, method='bisect')
-        # XXX Possible loss of precison in (1 - g):
-        return (dfd/dfn)*(1 - g)/g
+    dfn, dfd = _validate_params(dfn, dfd)
+    p = _validate_p(p)
+    g = betaincinv(dfd/2, dfn/2, p, method='bisect')
+    # XXX Possible loss of precison in (1 - g):
+    return (dfd/dfn)*(1 - g)/g
 
 
+@mp.extradps(5)
 def support(dfn, dfd):
     """
     Support of the F distribution.
     """
-    with mp.extradps(5):
-        _validate_params(dfn, dfd)
-        return (mp.zero, mp.inf)
+    _validate_params(dfn, dfd)
+    return (mp.zero, mp.inf)
 
 
+@mp.extradps(5)
 def mean(dfn, dfd):
     """
     Mean of the F distribution.
@@ -142,13 +142,13 @@ def mean(dfn, dfd):
     For a finite mean, `dfd` must be greater than 2.
     If `dfd` is less than or equal to 2, this function returns `inf`.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        if dfd > 2:
-            return dfd/(dfd - 2)
-        return mp.inf
+    dfn, dfd = _validate_params(dfn, dfd)
+    if dfd > 2:
+        return dfd/(dfd - 2)
+    return mp.inf
 
 
+@mp.extradps(5)
 def var(dfn, dfd):
     """
     Variance of the F distribution.
@@ -158,14 +158,14 @@ def var(dfn, dfd):
     For a finite variance, `dfd` must be greater than 4.
     If `dfd` is less than or equal to 4, this function returns `inf`.
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        if dfd > 4:
-            return (2 * dfd**2 * (dfn + dfd - 2)
-                    / (dfn * (dfd - 2)**2 * (dfd - 4)))
-        return mp.inf
+    dfn, dfd = _validate_params(dfn, dfd)
+    if dfd > 4:
+        return (2 * dfd**2 * (dfn + dfd - 2)
+                / (dfn * (dfd - 2)**2 * (dfd - 4)))
+    return mp.inf
 
 
+@mp.extradps(5)
 def entropy(dfn, dfd):
     """
     Differential entropy of the F distribution.
@@ -173,13 +173,12 @@ def entropy(dfn, dfd):
     `dfn` and `dfd` are the numerator and denominator degrees of freedom, resp.
 
     """
-    with mp.extradps(5):
-        dfn, dfd = _validate_params(dfn, dfd)
-        dfn2 = dfn/2
-        dfd2 = dfd/2
-        dfmean = dfn2 + dfd2
-        return (mp.log(dfd/dfn)
-                + logbeta(dfn2, dfd2)
-                + (1 - dfn2)*mp.digamma(dfn2)
-                - (1 + dfd2)*mp.digamma(dfd2)
-                + dfmean*mp.digamma(dfmean))
+    dfn, dfd = _validate_params(dfn, dfd)
+    dfn2 = dfn/2
+    dfd2 = dfd/2
+    dfmean = dfn2 + dfd2
+    return (mp.log(dfd/dfn)
+            + logbeta(dfn2, dfd2)
+            + (1 - dfn2)*mp.digamma(dfn2)
+            - (1 + dfd2)*mp.digamma(dfd2)
+            + dfmean*mp.digamma(dfmean))
