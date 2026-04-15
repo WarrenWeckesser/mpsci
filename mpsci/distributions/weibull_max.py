bc0bcc39821b05171eeb1aaddf1b73fa477217b3
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-15 14:02:06 -0400

    MAINT: Use the mp.extradps decorator in weibull_max.py and weibull_min.py

--- mpsci/distributions/weibull_max.py
+++ mpsci/distributions/weibull_max.py
@@ -36,6 +36,7 @@ __all__ = ['pdf', 'logpdf', 'cdf', 'invcdf', 'sf', 'invsf',
            'noncentral_moment', 'nll', 'mle']
 
 
+@mp.extradps(5)
 def pdf(x, k, loc, scale):
     """
     Probability density function for the Weibull distribution (for maxima).
@@ -43,21 +44,21 @@ def pdf(x, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k, loc, scale = _validate_params(k, loc, scale)
-        if x == loc:
-            if k < 1:
-                return mp.inf
-            elif k == 1:
-                return 1/scale
-            else:
-                return mp.zero
-        if x > loc:
+    x = mp.mpf(x)
+    k, loc, scale = _validate_params(k, loc, scale)
+    if x == loc:
+        if k < 1:
+            return mp.inf
+        elif k == 1:
+            return 1/scale
+        else:
             return mp.zero
-        return mp.exp(logpdf(x, k, loc, scale))
+    if x > loc:
+        return mp.zero
+    return mp.exp(logpdf(x, k, loc, scale))
 
 
+@mp.extradps(5)
 def logpdf(x, k, loc, scale):
     """
     Log of the PDF of the Weibull distribution (for maxima).
@@ -65,23 +66,23 @@ def logpdf(x, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k, loc, scale = _validate_params(k, loc, scale)
-        if x == loc:
-            if k < 1:
-                return mp.inf
-            elif k == 1:
-                return -mp.log(scale)
-            else:
-                return mp.ninf
-        if x > loc:
+    x = mp.mpf(x)
+    k, loc, scale = _validate_params(k, loc, scale)
+    if x == loc:
+        if k < 1:
+            return mp.inf
+        elif k == 1:
+            return -mp.log(scale)
+        else:
             return mp.ninf
-        z = (x - loc) / scale
-        return (mp.log(k) - k*mp.log(scale) +
-                (k - 1)*mp.log(-x+loc) - (-z)**k)
+    if x > loc:
+        return mp.ninf
+    z = (x - loc) / scale
+    return (mp.log(k) - k*mp.log(scale) +
+            (k - 1)*mp.log(-x+loc) - (-z)**k)
 
 
+@mp.extradps(5)
 def cdf(x, k, loc, scale):
     """
     Cumulative distribution function for the Weibull distribution (for maxima).
@@ -89,15 +90,15 @@ def cdf(x, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k, loc, scale = _validate_params(k, loc, scale)
-        if x >= loc:
-            return mp.one
-        z = (x - loc) / scale
-        return mp.exp(-(-z)**k)
+    x = mp.mpf(x)
+    k, loc, scale = _validate_params(k, loc, scale)
+    if x >= loc:
+        return mp.one
+    z = (x - loc) / scale
+    return mp.exp(-(-z)**k)
 
 
+@mp.extradps(5)
 def invcdf(p, k, loc, scale):
     """
     Inverse of the CDF for the Weibull distribution (for maxima).
@@ -105,14 +106,14 @@ def invcdf(p, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        p = _validate_p(p)
-        k, loc, scale = _validate_params(k, loc, scale)
-        z = -mp.power(-mp.log(p), 1/k)
-        x = scale*z + loc
-        return x
+    p = _validate_p(p)
+    k, loc, scale = _validate_params(k, loc, scale)
+    z = -mp.power(-mp.log(p), 1/k)
+    x = scale*z + loc
+    return x
 
 
+@mp.extradps(5)
 def sf(x, k, loc, scale):
     """
     Survival function for the Weibull distribution (for maxima).
@@ -120,15 +121,15 @@ def sf(x, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        k, loc, scale = _validate_params(k, loc, scale)
-        if x >= loc:
-            return mp.zero
-        z = (x - loc) / scale
-        return -mp.expm1(-(-z)**k)
+    x = mp.mpf(x)
+    k, loc, scale = _validate_params(k, loc, scale)
+    if x >= loc:
+        return mp.zero
+    z = (x - loc) / scale
+    return -mp.expm1(-(-z)**k)
 
 
+@mp.extradps(5)
 def invsf(p, k, loc, scale):
     """
     Inverse of the survival function for the Weibull distribution (for maxima).
@@ -136,14 +137,14 @@ def invsf(p, k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        p = _validate_p(p)
-        k, loc, scale = _validate_params(k, loc, scale)
-        z = -mp.power(-mp.log1p(-p), 1/k)
-        x = scale*z + loc
-        return x
+    p = _validate_p(p)
+    k, loc, scale = _validate_params(k, loc, scale)
+    z = -mp.power(-mp.log1p(-p), 1/k)
+    x = scale*z + loc
+    return x
 
 
+@mp.extradps(5)
 def support(k, loc, scale):
     """
     Support of the Weibull distribution (for maxima).
@@ -151,11 +152,11 @@ def support(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        return (mp.ninf, loc)
+    k, loc, scale = _validate_params(k, loc, scale)
+    return (mp.ninf, loc)
 
 
+@mp.extradps(5)
 def mode(k, loc, scale):
     """
     Mode of the Weibull distribution (for maxima).
@@ -163,12 +164,12 @@ def mode(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        m = scale * mp.power((k - 1)/k, 1/k) if k > 1 else 0
-        return loc - m
+    k, loc, scale = _validate_params(k, loc, scale)
+    m = scale * mp.power((k - 1)/k, 1/k) if k > 1 else 0
+    return loc - m
 
 
+@mp.extradps(5)
 def mean(k, loc, scale):
     """
     Mean of the Weibull distribution (for maxima).
@@ -176,11 +177,11 @@ def mean(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        return loc - scale * mp.gamma(1 + 1/k)
+    k, loc, scale = _validate_params(k, loc, scale)
+    return loc - scale * mp.gamma(1 + 1/k)
 
 
+@mp.extradps(5)
 def var(k, loc, scale):
     """
     Variance of the Weibull distribution (for maxima).
@@ -188,13 +189,13 @@ def var(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        v1 = 1 + 1/k
-        v2 = 1 + 2/k
-        return scale**2 * (mp.gamma(v2) - mp.gamma(v1)**2)
+    k, loc, scale = _validate_params(k, loc, scale)
+    v1 = 1 + 1/k
+    v2 = 1 + 2/k
+    return scale**2 * (mp.gamma(v2) - mp.gamma(v1)**2)
 
 
+@mp.extradps(5)
 def skewness(k, loc, scale):
     """
     Skewness of the Weibull distribution (for maxima).
@@ -202,14 +203,14 @@ def skewness(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        g1 = mp.gamma(1 + 1/k)
-        g2 = mp.gamma(1 + 2/k)
-        g3 = mp.gamma(1 + 3/k)
-        return -(g3 - 3*g1*g2 + 2*g1**3) / mp.power(g2 - g1**2, 1.5)
+    k, loc, scale = _validate_params(k, loc, scale)
+    g1 = mp.gamma(1 + 1/k)
+    g2 = mp.gamma(1 + 2/k)
+    g3 = mp.gamma(1 + 3/k)
+    return -(g3 - 3*g1*g2 + 2*g1**3) / mp.power(g2 - g1**2, 1.5)
 
 
+@mp.extradps(5)
 def kurtosis(k, loc, scale):
     """
     Excess kurtosis of the Weibull distribution (for minima).
@@ -217,16 +218,16 @@ def kurtosis(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        g1 = mp.gamma(1 + 1/k)
-        g2 = mp.gamma(1 + 2/k)
-        g3 = mp.gamma(1 + 3/k)
-        g4 = mp.gamma(1 + 4/k)
-        den = (g2 - g1**2)**2
-        return (-6*g1**4 + 12*g1**2*g2 - 3*g2**2 - 4*g1*g3 + g4) / den
+    k, loc, scale = _validate_params(k, loc, scale)
+    g1 = mp.gamma(1 + 1/k)
+    g2 = mp.gamma(1 + 2/k)
+    g3 = mp.gamma(1 + 3/k)
+    g4 = mp.gamma(1 + 4/k)
+    den = (g2 - g1**2)**2
+    return (-6*g1**4 + 12*g1**2*g2 - 3*g2**2 - 4*g1*g3 + g4) / den
 
 
+@mp.extradps(5)
 def entropy(k, loc, scale):
     """
     Differential entropy of the Weibull distribution (for maxima).
@@ -234,45 +235,45 @@ def entropy(k, loc, scale):
     This is a three-parameter version of the distribution.  The more typical
     two-parameter version has just the parameters k and scale.
     """
-    with mp.extradps(5):
-        k, loc, scale = _validate_params(k, loc, scale)
-        return mp.euler*(1 - 1/k) + mp.log(scale) - mp.log(k) + 1
+    k, loc, scale = _validate_params(k, loc, scale)
+    return mp.euler*(1 - 1/k) + mp.log(scale) - mp.log(k) + 1
 
 
+@mp.extradps(5)
 def _standard_noncentral_moment(n, k):
-    with mp.extradps(5):
-        if n == 0:
-            return mp.one
-        return (-1)**(n % 2) * mp.gamma(1 + mp.mpf(n)/k)
+    if n == 0:
+        return mp.one
+    return (-1)**(n % 2) * mp.gamma(1 + mp.mpf(n)/k)
 
 
+@mp.extradps(5)
 def noncentral_moment(n, k, loc=0, scale=1):
     """
     Noncentral moment of the Weibull distribution (for maxima).
 
     The value is also known as the raw moment.
     """
-    with mp.extradps(5):
-        _validate_moment_n(n)
-        k, loc, sigma = _validate_params(k, loc, scale)
-        if n == 0:
-            return mp.one
-        terms = [(mp.binomial(n, i) * mp.power(loc, n - i) * mp.power(scale, i)
-                  * _standard_noncentral_moment(i, k))
-                 for i in range(n + 1)]
-        return mp.fsum(terms)
+    _validate_moment_n(n)
+    k, loc, sigma = _validate_params(k, loc, scale)
+    if n == 0:
+        return mp.one
+    terms = [(mp.binomial(n, i) * mp.power(loc, n - i) * mp.power(scale, i)
+              * _standard_noncentral_moment(i, k))
+             for i in range(n + 1)]
+    return mp.fsum(terms)
 
 
+@mp.extradps(5)
 def nll(x, k, loc, scale):
     """
     Negative log-likelihood function for the Weibull(min) distribution.
     """
     _validate_params(k, loc, scale)
-    with mp.extradps(5):
-        x = _validate_x_bounds(x, high=loc, strict_high=True, highname='loc')
-        return -mp.fsum([logpdf(t, k, loc, scale) for t in x])
+    x = _validate_x_bounds(x, high=loc, strict_high=True, highname='loc')
+    return -mp.fsum([logpdf(t, k, loc, scale) for t in x])
 
 
+@mp.extradps(5)
 def mle(x, *, k=None, loc=None, scale=None):
     """
     Maximum likelihood estimate of the Weibull(max) distribution parameters.
@@ -283,44 +284,43 @@ def mle(x, *, k=None, loc=None, scale=None):
     """
     if loc is None:
         raise ValueError("The 'loc' parameter must be given explicitly.")
-    with mp.extradps(5):
-        x = _validate_x_bounds(x, high=loc, strict_high=True, highname='loc')
-        # Transform x to loc - x.  After that transformation, this
-        # function is the same as weibull_min. (XXX FIXME: There is some
-        # D.R.Y. clean up to be done here.)
-        loc = mp.mpmathify(loc)
-        x = [-t + loc for t in x]
+    x = _validate_x_bounds(x, high=loc, strict_high=True, highname='loc')
+    # Transform x to loc - x.  After that transformation, this
+    # function is the same as weibull_min. (XXX FIXME: There is some
+    # D.R.Y. clean up to be done here.)
+    loc = mp.mpmathify(loc)
+    x = [-t + loc for t in x]
 
-        if k is not None and k <= 0:
-            raise ValueError('k must be greater than 0')
-        if scale is not None and scale <= 0:
-            raise ValueError('scale must be greater than 0')
+    if k is not None and k <= 0:
+        raise ValueError('k must be greater than 0')
+    if scale is not None and scale <= 0:
+        raise ValueError('scale must be greater than 0')
 
-        if k is None and scale is None:
-            # Solve for k and scale.
-            # TO DO: Is there a better guess for k than 1?
-            k_hat = mp.findroot(lambda k: _mle_k_eqn1(k, x), 1.0)
-            scale_hat = pmean(x, p=k_hat)
-        elif k is not None and scale is None:
-            # Solve for scale.
-            k_hat = mp.mpf(k)
-            scale_hat = pmean(x, p=k_hat)
-        elif k is None and scale is not None:
-            # Solve for k only.
-            scale_hat = mp.mpf(scale)
-            # Use the formula for the median,
-            #    median = scale * ln(2)**(1/k)
-            # to derive the initial guess for k.
-            med = _median(x)
-            lnr = mp.log(med/scale_hat)
-            if lnr >= 0:
-                k0 = 1.0
-            else:
-                k0 = mp.log(mp.log(2))/lnr
-            k_hat = mp.findroot(lambda k: _mle_k_eqn2(k, x, scale_hat), k0)
+    if k is None and scale is None:
+        # Solve for k and scale.
+        # TO DO: Is there a better guess for k than 1?
+        k_hat = mp.findroot(lambda k: _mle_k_eqn1(k, x), 1.0)
+        scale_hat = pmean(x, p=k_hat)
+    elif k is not None and scale is None:
+        # Solve for scale.
+        k_hat = mp.mpf(k)
+        scale_hat = pmean(x, p=k_hat)
+    elif k is None and scale is not None:
+        # Solve for k only.
+        scale_hat = mp.mpf(scale)
+        # Use the formula for the median,
+        #    median = scale * ln(2)**(1/k)
+        # to derive the initial guess for k.
+        med = _median(x)
+        lnr = mp.log(med/scale_hat)
+        if lnr >= 0:
+            k0 = 1.0
         else:
-            # Both k and scale are not None--nothing to do.
-            k_hat = mp.mpf(k)
-            scale_hat = mp.mpf(scale)
+            k0 = mp.log(mp.log(2))/lnr
+        k_hat = mp.findroot(lambda k: _mle_k_eqn2(k, x, scale_hat), k0)
+    else:
+        # Both k and scale are not None--nothing to do.
+        k_hat = mp.mpf(k)
+        scale_hat = mp.mpf(scale)
 
     return k_hat, loc, scale_hat
