8ba2507edf3efae5ec2463cfa0b183932038da88
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-17 09:27:50 -0400

    MAINT: Use the mp.extradps decorator in pareto.py.

--- mpsci/distributions/pareto.py
+++ mpsci/distributions/pareto.py
@@ -29,131 +29,132 @@ def _validate_params(b, loc, scale):
     return mp.mpf(b), mp.mpf(loc), mp.mpf(scale)
 
 
+@mp.extradps(5)
 def pdf(x, b, loc=0, scale=1):
     """
     Probability density function for the Pareto distribution (type I).
 
     """
     b, loc, scale = _validate_params(b, loc, scale)
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        lb = loc + scale
-        if x < lb:
-            return mp.zero
-        z = (x - loc)/scale
-        return b*z**(-b - 1)/scale
+    x = mp.mpf(x)
+    lb = loc + scale
+    if x < lb:
+        return mp.zero
+    z = (x - loc)/scale
+    return b*z**(-b - 1)/scale
 
 
+@mp.extradps(5)
 def logpdf(x, b, loc=0, scale=1):
     """
     Logarithm of the PDF for the Pareto distribution (type I).
 
     """
     b, loc, scale = _validate_params(b, loc, scale)
-    with mp.extradps(5):
-        x = mp.mpf(x)
-        lb = loc + scale
-        if x < lb:
-            return mp.ninf
-        # z = (x - loc)/scale
-        return mp.log(b) - (b + 1)*mp.log(x - loc) + b*mp.log(scale)
+    x = mp.mpf(x)
+    lb = loc + scale
+    if x < lb:
+        return mp.ninf
+    # z = (x - loc)/scale
+    return mp.log(b) - (b + 1)*mp.log(x - loc) + b*mp.log(scale)
 
 
+@mp.extradps(5)
 def cdf(x, b, loc=0, scale=1):
     """
     Cumulative distribution function for the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        x = mp.mpf(x)
-        lb = loc + scale
-        if x < lb:
-            return mp.zero
-        z = (x - loc)/scale
-        return -mp.powm1(z, -b)
+    b, loc, scale = _validate_params(b, loc, scale)
+    x = mp.mpf(x)
+    lb = loc + scale
+    if x < lb:
+        return mp.zero
+    z = (x - loc)/scale
+    return -mp.powm1(z, -b)
 
 
+@mp.extradps(5)
 def invcdf(p, b, loc=0, scale=1):
     """
     Inverse of the CDF of the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        p = _validate_p(p)
-        return loc + scale*inv_powm1(-p, -b)
+    b, loc, scale = _validate_params(b, loc, scale)
+    p = _validate_p(p)
+    return loc + scale*inv_powm1(-p, -b)
 
 
+@mp.extradps(5)
 def sf(x, b, loc=0, scale=1):
     """
     Survival function for the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        x = mp.mpf(x)
-        lb = loc + scale
-        if x < lb:
-            return mp.one
-        z = (x - loc)/scale
-        return mp.power(z, -b)
+    b, loc, scale = _validate_params(b, loc, scale)
+    x = mp.mpf(x)
+    lb = loc + scale
+    if x < lb:
+        return mp.one
+    z = (x - loc)/scale
+    return mp.power(z, -b)
 
 
+@mp.extradps(5)
 def invsf(p, b, loc=0, scale=1):
     """
     Inverse of the survival function of the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        p = _validate_p(p)
-        return loc + scale*mp.power(p, -1/b)
+    b, loc, scale = _validate_params(b, loc, scale)
+    p = _validate_p(p)
+    return loc + scale*mp.power(p, -1/b)
 
 
+@mp.extradps(5)
 def support(b, loc=0, scale=1):
     """
     Support of the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        return (loc + scale, mp.inf)
+    b, loc, scale = _validate_params(b, loc, scale)
+    return (loc + scale, mp.inf)
 
 
+@mp.extradps(5)
 def mean(b, loc=0, scale=1):
     """
     Mean of the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        if b <= 1:
-            return mp.inf
-        return loc + scale*b/(b - 1)
+    b, loc, scale = _validate_params(b, loc, scale)
+    if b <= 1:
+        return mp.inf
+    return loc + scale*b/(b - 1)
 
 
+@mp.extradps(5)
 def var(b, *, loc=0, scale=1):
     """
     Variance of the Pareto distribution (type I).
 
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        if b <= 2:
-            return mp.nan
-        return scale**2*b/(b - 1)**2/(b - 2)
+    b, loc, scale = _validate_params(b, loc, scale)
+    if b <= 2:
+        return mp.nan
+    return scale**2*b/(b - 1)**2/(b - 2)
 
 
+@mp.extradps(5)
 def entropy(b, loc=0, scale=1):
     """
     Differential entropy of the Pareto distribution (type I).
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        return 1 + 1/b + mp.log(scale/b)
+    b, loc, scale = _validate_params(b, loc, scale)
+    return 1 + 1/b + mp.log(scale/b)
 
 
+@mp.extradps(5)
 def nll(x, b, loc=0, scale=1):
     """
     Negative log-likelihood function for the Pareto distribution (type I).
@@ -161,17 +162,17 @@ def nll(x, b, loc=0, scale=1):
     `x` must be a sequence of numbers, and each value in `x` must greater
     than or equal to `loc` + `scale`.
     """
-    with mp.extradps(5):
-        b, loc, scale = _validate_params(b, loc, scale)
-        x = _validate_x_bounds(x, low=loc+scale, strict_low=False,
-                               lowname='loc+scale')
-        return -mp.fsum(logpdf(t, b, loc=loc, scale=scale) for t in x)
+    b, loc, scale = _validate_params(b, loc, scale)
+    x = _validate_x_bounds(x, low=loc+scale, strict_low=False,
+                           lowname='loc+scale')
+    return -mp.fsum(logpdf(t, b, loc=loc, scale=scale) for t in x)
 
 
 def _is_fixed(obj):
     return obj is not None and not isinstance(obj, Initial)
 
 
+@mp.extradps(5)
 def mle(x, *, b=None, loc=None, scale=None):
     """
     Maximum likelihood estimation for the Pareto distribution (type I).
@@ -187,79 +188,78 @@ def mle(x, *, b=None, loc=None, scale=None):
     b_fixed = _is_fixed(b)
     loc_fixed = _is_fixed(loc)
     scale_fixed = _is_fixed(scale)
-    with mp.extradps(5):
-        n = len(x)
+    n = len(x)
 
-        if b_fixed and loc_fixed and scale_fixed:
-            # All parameters are fixed.
-            b_hat, loc_hat, scale_hat = _validate_params(b, loc, scale)
-            x = _validate_x_bounds(x, low=loc_hat + scale_hat, high=mp.inf)
+    if b_fixed and loc_fixed and scale_fixed:
+        # All parameters are fixed.
+        b_hat, loc_hat, scale_hat = _validate_params(b, loc, scale)
+        x = _validate_x_bounds(x, low=loc_hat + scale_hat, high=mp.inf)
+        return b_hat, loc_hat, scale_hat
+
+    if not b_fixed:
+        if not loc_fixed and not scale_fixed:
+            # All parameters are free.
+            x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
+            x1 = min(x)
+
+            def mle_eqns(b, scale):
+                s1 = mp.fsum([mp.log(t - x1 + scale) for t in x])
+                eq1 = n/b + n*mp.log(scale) - s1
+                s2 = mp.fsum([1/(t - x1 + scale) for t in x])
+                eq2 = n*b/scale - (b + 1)*s2
+                return eq1, eq2
+
+            b0 = b.initial if isinstance(b, Initial) else 1
+            scale0 = scale.initial if isinstance(scale, Initial) else 1
+            b_hat, scale_hat = mp.findroot(mle_eqns, [b0, scale0])
+            loc_hat = x1 - scale_hat
             return b_hat, loc_hat, scale_hat
 
-        if not b_fixed:
-            if not loc_fixed and not scale_fixed:
-                # All parameters are free.
-                x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
-                x1 = min(x)
-
-                def mle_eqns(b, scale):
-                    s1 = mp.fsum([mp.log(t - x1 + scale) for t in x])
-                    eq1 = n/b + n*mp.log(scale) - s1
-                    s2 = mp.fsum([1/(t - x1 + scale) for t in x])
-                    eq2 = n*b/scale - (b + 1)*s2
-                    return eq1, eq2
-
-                b0 = b.initial if isinstance(b, Initial) else 1
-                scale0 = scale.initial if isinstance(scale, Initial) else 1
-                b_hat, scale_hat = mp.findroot(mle_eqns, [b0, scale0])
-                loc_hat = x1 - scale_hat
-                return b_hat, loc_hat, scale_hat
-
-            if not scale_fixed:
-                # b and scale are free, loc is fixed.
-                loc = mp.mpf(loc)
-                x = _validate_x_bounds(x, low=loc, high=mp.inf)
-                x1 = min(x)
-                scale_hat = x1 - loc
-                s1 = mp.fsum([mp.log(t - loc) for t in x])
-                b_hat = 1/(s1/n - mp.log(scale_hat))
-                return b_hat, loc, scale_hat
-
-            # b and loc are free, scale is fixed.
-            _, _, scale = _validate_params(1, 1, scale)
-            x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
-            x1 = min(x)
-            loc_hat = x1 - scale
-            s1 = mp.fsum([mp.log(t - loc_hat) for t in x])
-            b_hat = 1/(s1/n - mp.log(scale))
-            return b_hat, loc_hat, scale
-
-        # b is fixed
-        if not loc_fixed and not scale_fixed:
-            # b is fixed, loc and scale are free.
-            b, _, _ = _validate_params(b, 0, 1)
-            x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
-            x1 = min(x)
-
-            def mle_eqn(scale):
-                s = mp.fsum([1/(t - x1 + scale) for t in x])
-                return n*b/scale - (b + 1)*s
-
-            scale0 = scale.initial if isinstance(scale, Initial) else 1
-            scale_hat = mp.findroot(mle_eqn, scale0)
-            loc_hat = x1 - scale_hat
-            return b, loc_hat, scale_hat
-
-        if loc_fixed:
-            # b and loc are fixed, scale is free.
-            b, loc, _ = _validate_params(b, loc, 1)
+        if not scale_fixed:
+            # b and scale are free, loc is fixed.
+            loc = mp.mpf(loc)
             x = _validate_x_bounds(x, low=loc, high=mp.inf)
             x1 = min(x)
             scale_hat = x1 - loc
-            return b, loc, scale_hat
+            s1 = mp.fsum([mp.log(t - loc) for t in x])
+            b_hat = 1/(s1/n - mp.log(scale_hat))
+            return b_hat, loc, scale_hat
 
-        # b and scale are fixed, loc is free.
-        b, _, scale = _validate_params(b, 0, scale)
-        x1 = min([mp.mpf(t) for t in x])
+        # b and loc are free, scale is fixed.
+        _, _, scale = _validate_params(1, 1, scale)
+        x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
+        x1 = min(x)
         loc_hat = x1 - scale
-        return b, loc_hat, scale
+        s1 = mp.fsum([mp.log(t - loc_hat) for t in x])
+        b_hat = 1/(s1/n - mp.log(scale))
+        return b_hat, loc_hat, scale
+
+    # b is fixed
+    if not loc_fixed and not scale_fixed:
+        # b is fixed, loc and scale are free.
+        b, _, _ = _validate_params(b, 0, 1)
+        x = _validate_x_bounds(x, low=mp.ninf, high=mp.inf)
+        x1 = min(x)
+
+        def mle_eqn(scale):
+            s = mp.fsum([1/(t - x1 + scale) for t in x])
+            return n*b/scale - (b + 1)*s
+
+        scale0 = scale.initial if isinstance(scale, Initial) else 1
+        scale_hat = mp.findroot(mle_eqn, scale0)
+        loc_hat = x1 - scale_hat
+        return b, loc_hat, scale_hat
+
+    if loc_fixed:
+        # b and loc are fixed, scale is free.
+        b, loc, _ = _validate_params(b, loc, 1)
+        x = _validate_x_bounds(x, low=loc, high=mp.inf)
+        x1 = min(x)
+        scale_hat = x1 - loc
+        return b, loc, scale_hat
+
+    # b and scale are fixed, loc is free.
+    b, _, scale = _validate_params(b, 0, scale)
+    x1 = min([mp.mpf(t) for t in x])
+    loc_hat = x1 - scale
+    return b, loc_hat, scale
