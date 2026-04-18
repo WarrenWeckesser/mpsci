d33d1df286a85e79cb5a8d85eff2711cf84ca187
Warren Weckesser <warren.weckesser@gmail.com>
2026-04-17 20:02:40 -0400

    MAINT: Use the mp.extradps decorator consistently in test_loggamma.py.

--- mpsci/distributions/tests/test_loggamma.py
+++ mpsci/distributions/tests/test_loggamma.py
@@ -18,20 +18,20 @@ def test_pdf():
     assert mp.almosteq(p, mp.mpf(val))
 
 
+@mp.workdps(50)
 def test_basic_cdf_sf():
-    with mp.workdps(50):
-        k = 2
-        theta = 7
-        x = 1
-        p = loggamma.cdf(x, k, theta)
-        # From Wolfram Alpha:
-        #    CDF[ExpGammaDistribution[2, 7, 0], 1]
-        val = '0.3205287717282638332374539773545351545714864926297322725929829'
-        expected = mp.mpf(val)
-        assert mp.almosteq(p, expected)
+    k = 2
+    theta = 7
+    x = 1
+    p = loggamma.cdf(x, k, theta)
+    # From Wolfram Alpha:
+    #    CDF[ExpGammaDistribution[2, 7, 0], 1]
+    val = '0.3205287717282638332374539773545351545714864926297322725929829'
+    expected = mp.mpf(val)
+    assert mp.almosteq(p, expected)
 
-        p = loggamma.sf(x, k, theta)
-        assert mp.almosteq(p, 1 - expected)
+    p = loggamma.sf(x, k, theta)
+    assert mp.almosteq(p, 1 - expected)
 
 
 @pytest.mark.parametrize('funcpair', [(loggamma.cdf, loggamma.invcdf),
@@ -39,15 +39,16 @@ def test_basic_cdf_sf():
 @pytest.mark.parametrize('params',
                          [(1, 1), (0.125, 1.5), (1.25, 0.75)])
 @pytest.mark.parametrize('x0', [-2, -1, 0, 1, 2])
+@mp.workdps(50)
 def test_dist_roundtrip(funcpair, params, x0):
-    with mp.workdps(50):
-        func, invfunc = funcpair
-        k, theta = params
-        p = func(x0, k, theta)
-        x1 = invfunc(p, k, theta)
-        assert mp.almosteq(x1, x0)
+    func, invfunc = funcpair
+    k, theta = params
+    p = func(x0, k, theta)
+    x1 = invfunc(p, k, theta)
+    assert mp.almosteq(x1, x0)
 
 
+@mp.workdps(50)
 def test_interval_prob():
     p = loggamma.interval_prob(1, 4, 1.5, 3)
     # From Wolfram Alpha:
@@ -56,16 +57,16 @@ def test_interval_prob():
     assert mp.almosteq(p, mp.mpf(val))
 
 
+@mp.workdps(50)
 def test_mean():
-    with mp.workdps(50):
-        k = mp.mpf(2)
-        theta = mp.mpf(7)
-        mean = loggamma.mean(k, theta)
-        # Expected value computed with Wolfram Alpha:
-        #     Mean[ExpGammaDistribution[2, 7, 0]]
-        valstr = '2.9594903456892699757544153694231829827048846484205348083596'
-        expected = mp.mpf(valstr)
-        assert mp.almosteq(mean, expected)
+    k = mp.mpf(2)
+    theta = mp.mpf(7)
+    mean = loggamma.mean(k, theta)
+    # Expected value computed with Wolfram Alpha:
+    #     Mean[ExpGammaDistribution[2, 7, 0]]
+    valstr = '2.9594903456892699757544153694231829827048846484205348083596'
+    expected = mp.mpf(valstr)
+    assert mp.almosteq(mean, expected)
 
 
 @pytest.mark.parametrize('k, theta',
@@ -86,40 +87,40 @@ def test_mode(k, theta):
     assert loggamma.pdf(right, k, theta) < pm
 
 
+@mp.workdps(50)
 def test_var():
-    with mp.workdps(50):
-        k = mp.mpf(2)
-        theta = mp.mpf(7)
-        var = loggamma.var(k, theta)
-        # Expected value computed with Wolfram Alpha:
-        #     Variance[ExpGammaDistribution[2, 7, 0]]
-        valstr = '31.601769275563095387148343165655234271728545159133123449042'
-        expected = mp.mpf(valstr)
-        assert mp.almosteq(var, expected)
+    k = mp.mpf(2)
+    theta = mp.mpf(7)
+    var = loggamma.var(k, theta)
+    # Expected value computed with Wolfram Alpha:
+    #     Variance[ExpGammaDistribution[2, 7, 0]]
+    valstr = '31.601769275563095387148343165655234271728545159133123449042'
+    expected = mp.mpf(valstr)
+    assert mp.almosteq(var, expected)
 
 
+@mp.workdps(50)
 def test_skewness():
-    with mp.workdps(50):
-        k = mp.mpf(2)
-        theta = mp.mpf(7)
-        skew = loggamma.skewness(k, theta)
-        # Expected value computed with Wolfram Alpha:
-        #     Skewness[ExpGammaDistribution[2, 7, 0]]
-        valstr = '-0.780244491437787061530103769675602605070996940757461555988'
-        expected = mp.mpf(valstr)
-        assert mp.almosteq(skew, expected)
+    k = mp.mpf(2)
+    theta = mp.mpf(7)
+    skew = loggamma.skewness(k, theta)
+    # Expected value computed with Wolfram Alpha:
+    #     Skewness[ExpGammaDistribution[2, 7, 0]]
+    valstr = '-0.780244491437787061530103769675602605070996940757461555988'
+    expected = mp.mpf(valstr)
+    assert mp.almosteq(skew, expected)
 
 
+@mp.workdps(50)
 def test_kurtosis():
-    with mp.workdps(50):
-        k = mp.mpf(2)
-        theta = mp.mpf(7)
-        kurt = loggamma.kurtosis(k, theta)
-        # Expected value computed with Wolfram Alpha:
-        #     ExcessKurtosis[ExpGammaDistribution[2, 7, 0]]
-        valstr = '1.1875257511965620472368652875718220772212082979314426565409'
-        expected = mp.mpf(valstr)
-        assert mp.almosteq(kurt, expected)
+    k = mp.mpf(2)
+    theta = mp.mpf(7)
+    kurt = loggamma.kurtosis(k, theta)
+    # Expected value computed with Wolfram Alpha:
+    #     ExcessKurtosis[ExpGammaDistribution[2, 7, 0]]
+    valstr = '1.1875257511965620472368652875718220772212082979314426565409'
+    expected = mp.mpf(valstr)
+    assert mp.almosteq(kurt, expected)
 
 
 @pytest.mark.parametrize(
