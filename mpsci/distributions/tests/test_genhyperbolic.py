86a5dc25b2543bded7650322b3cf64c68b3c4220
Warren Weckesser <warren.weckesser@gmail.com>
2026-05-22 09:15:18 -0400

    TST: Add test of genhyperbolic.var()

--- mpsci/distributions/tests/test_genhyperbolic.py
+++ mpsci/distributions/tests/test_genhyperbolic.py
@@ -18,12 +18,17 @@ def test_pdf_is_normalized():
 
 
 @mp.workdps(40)
-def test_mean_with_integral():
+def test_mean_var_with_integral():
     p = 0.25
     a = 2.5
     b = 0.75
     loc = 0
     scale = 5
+
     m = gh.mean(p, a, b, loc=loc, scale=scale)
-    q = noncentral_moment_with_integral(1, gh, (p, a, b, loc, scale))
-    assert mp.almosteq(m, q)
+    mu1 = noncentral_moment_with_integral(1, gh, (p, a, b, loc, scale))
+    assert mp.almosteq(m, mu1)
+
+    v = gh.var(p, a, b, loc=loc, scale=scale)
+    mu2 = noncentral_moment_with_integral(2, gh, (p, a, b, loc, scale))
+    assert mp.almosteq(v, mu2 - mu1**2)
